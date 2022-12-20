e12b94b7530580ad3a7c99088d328a03c39c0521
brunaccampos <76260238+brunaccampos@users.noreply.github.com>
2022-12-20 10:15:52 -0300

    Add new config files

---
+++ Config Files/ManufacturedSolutionL3_v3.m
@@ -0,0 +1,158 @@
+function [Material, MeshU, MeshP, MeshN, BC, Control] = ManufacturedSolutionL3_v3(config_dir, progress_on)
+% ------------------------------------------------------------------------
+% Manufactured solution for L3 element mesh size convergence study
+% ux = x^5 - x^4
+% ------------------------------------------------------------------------
+% Adapted from https://github.com/GCMLab (Acknowledgements: Bruce Gee)
+% ------------------------------------------------------------------------
+
+%% Poroelasticity
+% porous media permeability [m2/Pa s]
+Material.kf = 0;
+% 1/Q (related to storage coefficient)
+Material.Minv = 0;
+% Biot's coefficient
+Material.alpha = 0;
+% poroelasticity model
+Control.Biotmodel = 1;
+
+% constititive law - 'PlaneStress' or 'PlaneStrain'
+% Note: use 'PlaneStrain' for 1D or 2D poroelasticity
+Material.constLaw = 'PlaneStress';
+
+%% Material properties
+% elasticity modulus [Pa]
+Material.E = 1;
+% Poisson's ratio
+Material.nu = 0.3;
+
+%% Mesh parameters
+if progress_on
+    disp([num2str(toc),': Building Mesh...']);
+end
+
+% mesh type
+% 'Manual': 1D mesh
+% 'Gmsh': 2D mesh, input file from GMSH
+MeshType = 'Manual';
+
+switch MeshType
+    case 'Manual'
+        % number of space dimensions
+        nsd = 1;
+        % number of elements
+        ne = 128;
+        % column size [m]
+        L = 1;
+        
+        % solid displacement field
+        typeU = 'L2';
+        fieldU = 'u';
+        MeshU = Build1DMesh(nsd, ne, L, typeU, fieldU);
+        
+        % fluid pressure field
+        typeP = 'L2';
+        fieldP = 'p';
+        MeshP = Build1DMesh(nsd, ne, L, typeP, fieldP);
+        
+        MeshN = [];
+        
+    case 'Gmsh'
+        % Version 2 ASCII
+        % number of space dimensions
+        nsd = 2;
+        %%%% displacement field
+        fieldU = 'u';
+        % build mesh displacement field
+        meshFileNameU = 'Mesh Files\Manufactured_finerQ4.msh';
+        MeshU = BuildMesh_GMSH(meshFileNameU, fieldU, nsd, config_dir, progress_on);
+        %%%% pressure field
+        fieldP = 'p';
+        % build mesh pressure field
+        meshFileNameP = 'Mesh Files\Manufactured_finerQ4.msh';
+        MeshP = BuildMesh_GMSH(meshFileNameP, fieldP, nsd, config_dir, progress_on);
+        %%%% porosity field
+        MeshN = [];
+end
+
+%% Initial conditions
+% displacement
+BC.initU = [];
+
+% pressure
+BC.initP = [];
+
+%% Find nodes for prescribed BCs
+% find top and bottom nodes for displacement field
+BC.top_node_u = find(MeshU.coords == max(MeshU.coords));
+BC.bottom_node_u = find(MeshU.coords == min(MeshU.coords));
+
+% find top and bottom nodes for pressure field
+BC.top_node_p = find(MeshP.coords == max(MeshP.coords));
+BC.bottom_node_p = find(MeshP.coords == min(MeshP.coords));
+
+%% Dirichlet BCs - solid
+BC.ux = @(x) -2*x.^4 + 8*max(MeshU.coords)^2.*x;
+% column vector of prescribed displacement dof
+BC.fixed_u_dof1 = BC.top_node_u;
+BC.fixed_u_dof2 = BC.bottom_node_u;
+BC.fixed_u = [BC.fixed_u_dof1; BC.fixed_u_dof2];
+% prescribed displacement
+BC.fixed_u_value = zeros(length(BC.fixed_u),1);
+BC.fixed_u_value = BC.ux(MeshU.coords(BC.fixed_u));
+% free displacement nodes
+BC.free_u = setdiff(MeshU.DOF, BC.fixed_u);
+
+%% Dirichlet BCs - fluid
+% prescribed pressure
+BC.fixed_p = 1:MeshP.nDOF;
+BC.fixed_p_value = zeros(length(BC.fixed_p),1);
+% free pressure nodes
+BC.free_p = setdiff(MeshP.DOF, BC.fixed_p);
+
+%% Neumann BCs - solid
+% column vector of prescribed traction nodes
+BC.tractionNodes = [];
+
+% body force
+BC.b = @(x) 24*x.^2;
+
+% point load [N]
+BC.pointLoad = [];
+
+%% Neumann BCs - fluid
+% point flux [m/s]
+BC.pointFlux = [];
+
+% distributed flux [m/s]
+BC.fluxNodes = [];
+
+% flux source
+BC.s = @(x)[]; 
+
+%% Quadrature order
+Control.nqU = 2;
+Control.nqP = 2;
+
+%% Problem type
+% 1 = quasi-steady/transient problem (no acceleration and pressure change)
+% 0 = dynamic problem (acceleration/intertia terms included)
+Control.steady = 1;
+
+%% Solution parameters
+Control.dt = 1;  % time step
+Control.tend = 1;
+Control.step = 1;
+
+Control.beta = 1; % beta-method time discretization -- beta = 1 Backward Euler; beta = 0.5 Crank-Nicolson
+
+Control.plotu = round(MeshU.nn/2);
+Control.plotp = round(MeshP.nn/2);
+
+% plot analytical solution (valid for 1D problems with Material.Minv == 0)
+Control.plotansol = 1; % 1 = true; 0 = false
+
+% solve in the frequency domain
+Control.freqDomain = 0;  % 1 = true; 0 = false
+
+end
