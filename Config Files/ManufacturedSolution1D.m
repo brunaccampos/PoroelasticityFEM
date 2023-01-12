78303a688cb7b019b91f47756c0e4a81cc2e9f61
Bruna <bccampos@uwaterloo.ca>
2023-01-12 09:39:36 -0500

    Change config file name

--- Config Files/ManufacturedSolution1D.m
+++ Config Files/ManufacturedSolution1D.m
@@ -1,4 +1,4 @@
-function [Material, MeshU, MeshP, MeshN, BC, Control] = ManufacturedSolutionL3(config_dir, progress_on)
+function [Material, MeshU, MeshP, MeshN, BC, Control] = ManufacturedSolution1D(config_dir, progress_on)
 % ------------------------------------------------------------------------
 % Manufactured solution for L3 element mesh size convergence study
 % ux = x^5 - x^4
