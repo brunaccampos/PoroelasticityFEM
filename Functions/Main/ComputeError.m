76158f6e59a30331838fa9b5c61d63a7d63d95ef
Bruna <bccampos@uwaterloo.ca>
2023-01-25 09:22:57 -0500

    Change folder of compute error files

--- Functions/Main/ComputeError.m
+++ Functions/Main/ComputeError.m
@@ -8,11 +8,11 @@ clear, clc
 close all
 
 %% Number of sims
-nsims = 5;
+nsims = 4;
 
 %% Mesh 1
 % load file
-load Results_m1L3_v2.mat
+load Results_m1_space_Boone.mat
 % mesh
 MeshP1 = MeshP;
 MeshU1 = MeshU;
@@ -24,7 +24,7 @@ dp_exact1 = p_an;
 du_exact1 = u_an;
 
 %% Mesh 2
-load Results_m2L3_v2.mat
+load Results_m2_space_Boone.mat
 % mesh
 MeshP2 = MeshP;
 MeshU2 = MeshU;
@@ -36,7 +36,7 @@ dp_exact2 = p_an;
 du_exact2 = u_an;
 
 %% Mesh 3
-load Results_m3L3_v2.mat
+load Results_m3_space_Boone.mat
 % mesh
 MeshP3 = MeshP;
 MeshU3 = MeshU;
@@ -48,7 +48,7 @@ dp_exact3 = p_an;
 du_exact3 = u_an;
 
 %% Mesh 4
-load Results_m4L3_v2.mat
+load Results_m4_space_Boone.mat
 % mesh
 MeshP4 = MeshP;
 MeshU4 = MeshU;
@@ -60,16 +60,105 @@ dp_exact4 = p_an;
 du_exact4 = u_an;
 
 %% Mesh 5
-load Results_m5L3_v2.mat
-% mesh
-MeshP5 = MeshP;
-MeshU5 = MeshU;
-% FEM approximation
-dp5 = p;
-du5 = u;
-% exact solution
-dp_exact5 = p_an;
-du_exact5 = u_an;
+% load Results_m5_space_Boone.mat
+% % mesh
+% MeshP5 = MeshP;
+% MeshU5 = MeshU;
+% % FEM approximation
+% dp5 = p;
+% du5 = u;
+% % exact solution
+% dp_exact5 = p_an;
+% du_exact5 = u_an;
+
+
+
+
+% shear modulus [GPa]
+Material.G = 6;
+% Poisson's ratio
+Material.nu = 0.2;
+% elasticity modulus [GPa]
+Material.E = 2 * Material.G * (1 + Material.nu);
+% porous media permeability [m2/GPa s]
+Material.kf = 2e-2;
+% dynamic viscosity [GPa s]
+Material.mu = 1e-12;
+% intrinsic permeability [m2]
+Material.k = Material.kf * Material.mu;
+% Biot's coefficient
+Material.alpha = 1;
+% fluid bulk modulus [GPa]
+Material.Kf = 3;
+% solid bulk modulus [GPa]
+Material.Ks = 36;
+% fluid bulk viscosity [GPa s]
+Material.xif = 2.8e-12; % (Quiroga-Goode, 2005)
+% material porosity
+Material.n = 0.19;
+% 1/Q (related to storage coefficient)
+Material.Minv = (Material.alpha - Material.n)/Material.Ks + Material.n/Material.Kf;
+
+% additional coefficients for analytical result
+% Lame constant [GPa]
+Material.lambda = Material.E * Material.nu/((1+Material.nu)*(1-2*Material.nu));
+% gravitational acceleration [m/s2]
+Material.g = 9.81;
+% fluid density [10^9 kg/m3]
+Material.rho_f = 1000e-9;
+% hydraulic conductivity [m/s]
+Material.kh = Material.kf * Material.rho_f * Material.g;
+
+
+
+
+
+% column length
+L = max(MeshU.coords);
+
+% coordinates vector
+% xu = MeshU.coords;
+% xp = MeshP.coords;
+
+syms x
+
+% traction
+P = 1e-6;
+% time
+t = 10;
+
+% material parameters
+mu = Material.G;
+M = 1/Material.Minv;
+alpha = Material.alpha;
+Ku = Material.lambda + 2*mu/3 + alpha^2*M;
+cm = 1/(Material.lambda + 2*mu);
+c = Material.kh/(Material.rho_f*Material.g*(Material.Minv + alpha^2*cm));
+
+% initial displacement after instantaneous load
+u0 = P*(L-x)/(Ku + 4*mu/3);
+% initial pressure after instantaneous load
+p0 = alpha*M*P/(Ku + 4*mu/3);
+
+% number of terms for Fourier expansion
+N = 100;
+
+% auxiliar terms
+auxp = 0;
+auxu = 0;
+
+% loop over N
+for m = 0:N
+   auxp = auxp + (1/(2*m+1)) * exp(-(2*m+1)^2*pi()^2*c*t/(4*L^2)) * sin((2*m+1)*pi().*x/(2*L));
+   auxu = auxu + (1/(2*m+1)^2) * exp(-(2*m+1)^2*pi()^2*c*t/(4*L^2)) * cos((2*m+1)*pi().*x/(2*L));
+end
+
+p_an = 4*p0.*auxp/pi();
+u_an = cm*p0*(L-x -8*L.*auxu/pi()^2) + u0;
+
+
+
+
 
 %% Compute L2 error norm
 % initialize variables
@@ -81,6 +170,7 @@ normp = zeros(nsims,1);
 
 % loop over meshes
 for sim = 1:nsims
+    
     switch sim
         case 1
             dp = dp1;
@@ -90,7 +180,7 @@ for sim = 1:nsims
             MeshP = MeshP1;
             MeshU = MeshU1;
             normu(sim,1) = norm(du-du_exact,2);
-            normp(sim,1) = norm(dp-dp_exact,'inf');
+            normp(sim,1) = norm(dp-dp_exact,2);
         case 2
             dp = dp2;
             du = du2;
@@ -99,7 +189,7 @@ for sim = 1:nsims
             MeshP = MeshP2;
             MeshU = MeshU2;
             normu(sim,1) = norm(du-du_exact,2);
-            normp(sim,1) = norm(dp-dp_exact,'inf');
+            normp(sim,1) = norm(dp-dp_exact,2);
         case 3
             dp = dp3;
             du = du3;
@@ -108,7 +198,7 @@ for sim = 1:nsims
             MeshP = MeshP3;
             MeshU = MeshU3;
             normu(sim,1) = norm(du-du_exact,2);
-            normp(sim,1) = norm(dp-dp_exact,'inf');
+            normp(sim,1) = norm(dp-dp_exact,2);
         case 4
             dp = dp4;
             du = du4;
@@ -117,7 +207,7 @@ for sim = 1:nsims
             MeshP = MeshP4;
             MeshU = MeshU4;
             normu(sim,1) = norm(du-du_exact,2);
-            normp(sim,1) = norm(dp-dp_exact,'inf');
+            normp(sim,1) = norm(dp-dp_exact,2);
         case 5
             dp = dp5;
             du = du5;
@@ -126,7 +216,7 @@ for sim = 1:nsims
             MeshP = MeshP5;
             MeshU = MeshU5;
             normu(sim,1) = norm(du-du_exact,2);
-            normp(sim,1) = norm(dp-dp_exact,'inf');
+            normp(sim,1) = norm(dp-dp_exact,2);
         case 6
             dp = dp6;
             du = du6;
@@ -135,7 +225,7 @@ for sim = 1:nsims
             MeshP = MeshP6;
             MeshU = MeshU6;
             normu(sim,1) = norm(du-du_exact,2);
-            normp(sim,1) = norm(dp-dp_exact,'inf');
+            normp(sim,1) = norm(dp-dp_exact,2);
     end
     
     % Mesh size
@@ -152,12 +242,9 @@ for sim = 1:nsims
     end
     
     % exact solution
-%     [p_an, u_an] = getAnalyticResult_v3(MeshU, MeshP, Control);
 %     d_exact = @(x) x.^5 - x.^4;
-    d_exact = @(x) -x.^2 + 2*max(MeshU.coords).*x;
-%     d_exact = @(x) -x.^3 + x.^2 +(3*max(MeshU.coords)^2 - 2*max(MeshU.coords)).*x;
 
-            Control.nqU = 8;
+    Control.nqU = 8;
     MeshU.field = 'u';
     Quad = GlobalQuad(MeshU, Control);
     
@@ -204,7 +291,7 @@ for sim = 1:nsims
 %                     uxe_p = eval(subs(p_an, Np*MeshP.coords(connp_e)));
 %                     uxe_u = eval(subs(u_an, Nu*MeshU.coords(connu_e)));
 
-                    uxe_u = eval(subs(d_exact, Nu*MeshU.coords(connu_e)));
+%                     uxe_u = eval(subs(d_exact, Nu*MeshU.coords(connu_e)));
 
                 case 2
                     % approximated displacement at quadrature point
@@ -239,10 +326,10 @@ m_L2p = pL2p(1);
 pL2u = polyfit(log(h(1:2)), log(eL2u(1:2)),1);
 m_L2u = pL2u(1);
 
-pL2p2 = polyfit(log(h(1:2)), log(normp(1:2)),1);
+pL2p2 = polyfit(log(h), log(normp),1);
 m_L2p2 = pL2p2(1);
 
-pL2u2 = polyfit(log(h(1:2)), log(normu(1:2)),1);
+pL2u2 = polyfit(log(h), log(normu),1);
 m_L2u2 = pL2u2(1);
 
 
