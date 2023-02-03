8d9bc863e49cbb0a182dd9daf4ae1541dabbcce5
Bruna <bccampos@uwaterloo.ca>
2023-02-03 12:43:54 -0500

    Add H1 error norm

--- Functions/Post Processing/ComputeMeshSizeError.m
+++ Functions/Post Processing/ComputeMeshSizeError.m
@@ -24,11 +24,37 @@ end
 % approximate solutions
 du = Solution.u;
 dp = Solution.p;
+e = Solution.e;
+s = Solution.s;
+q = Solution.q;
 
 % exact solutions evaluated at nodes
 du_exact = Plot.uan_space;
 dp_exact = Plot.pan_space;
+e_exact = Solution.e_an;
+s_exact = Solution.s_an;
+q_exact = Solution.q_an;
 
+% d_exact = @(x) x.^5 - x.^4;
+% e_exact = @(x) 5*x.^4 - 4*x.^3;
+% s_exact = e_exact;
+
+% p_exact = @(x) x.^5 - x.^4;
+% qex_exact = @(x)-( 5*x.^4 - 4*x.^3);
+
+% syms x
+% aux=0;
+% % x = MeshP.coords;
+% N=1000;
+% % for k=1:N
+% %     aux = aux + (1/k)*exp(-3*k^2*pi()^2*10)*sin(k*pi()*x);
+% % end
+% % 
+% % p_exact = 100 - 50*x - (100/pi()) * aux;
+% p_exact = 100 - 50*x;
+% 
+        
+        
 % higher quadrature to evaluate error
 Control.nqU = 8;
 Quad = GlobalQuad(MeshU, Control);
@@ -38,6 +64,16 @@ eL2u_num = 0;
 eL2u_den = 0;
 eL2p_num = 0;
 eL2p_den = 0;
+
+eENu_num = 0;
+eENu_den = 0;
+eENp_num = 0;
+eENp_den = 0;
+
+eH1u_num = 0;
+eH1u_den = 0;
+eH1p_num = 0;
+eH1p_den = 0;
 
 %% Compute error norm
 % loop over elements
@@ -64,47 +100,85 @@ for i = 1:ne
        switch MeshP.nsd
            case 1 % 1D
                % approximate (FEM)
-               uxh_u = Nu*du(MeshU.xdofs(connu_e)');
-               uyh_u = zeros(length(uxh_u));
+               uxh = Nu*du(MeshU.xdofs(connu_e)');
+               uyh = zeros(length(uxh));
                
-               uxh_p = Np*dp(MeshP.xdofs(connp_e)');
-               uyh_p = zeros(length(uxh_p));
+               pxh = Np*dp(MeshP.xdofs(connp_e)');
+               pyh = zeros(length(pxh));
                
                % exact
-               uxe_u = Nu*du_exact(MeshU.xdofs(connu_e)');
-               uye_u = zeros(length(uxe_u));
+               uxe = Nu*du_exact(MeshU.xdofs(connu_e)');
+               uye = zeros(length(uxe));
                
-               uxe_p = Np*dp_exact(MeshP.xdofs(connp_e)');
-               uye_p = zeros(length(uxe_p));
+               pxe = Np*dp_exact(MeshP.xdofs(connp_e)');
+               pye = zeros(length(pxe));
+               
+               % with function handle
+%                uxe = eval(subs(d_exact, Nu*MeshU.coords(connu_e)));
+%                pxe = eval(subs(p_exact, Np*MeshP.coords(connp_e)));
                
            case 2 % 2D
                % approximate (FEM)
-               uxh_u = Nu*du(MeshU.xdofs(connu_e)');
-               uyh_u = Nu*du(MeshU.ydofs(connu_e)');
+               uxh = Nu*du(MeshU.xdofs(connu_e)');
+               uyh = Nu*du(MeshU.ydofs(connu_e)');
                
-               uxh_p = Np*dp(MeshP.xdofs(connp_e)');
-               uyh_p = Np*dp(MeshP.ydofs(connp_e)');
+               pxh = Np*dp(MeshP.xdofs(connp_e)');
+               pyh = Np*dp(MeshP.ydofs(connp_e)');
                
                % exact
-               uxe_u = Nu*du_exact(MeshU.xdofs(connu_e)');
-               uye_u = Nu*du_exact(MeshU.ydofs(connu_e)');
+               uxe = Nu*du_exact(MeshU.xdofs(connu_e)');
+               uye = Nu*du_exact(MeshU.ydofs(connu_e)');
                
-               uxe_p = Np*dp_exact(MeshP.xdofs(connp_e)');
-               uye_p = Np*dp_exact(MeshP.ydofs(connp_e)'); 
+               pxe = Np*dp_exact(MeshP.xdofs(connp_e)');
+               pye = Np*dp_exact(MeshP.ydofs(connp_e)'); 
        end
        
        % L2 norm displacement
-       eL2u_num = eL2u_num + [uxh_u - uxe_u, uyh_u - uye_u] * [uxh_u - uxe_u; uyh_u - uye_u] * Quad.w(ip,1) * Jdet;
-       eL2u_den = eL2u_den + [uxe_u, uye_u] * [uxe_u; uye_u] * Quad.w(ip,1) * Jdet;
+       eL2u_num = eL2u_num + [uxh - uxe, uyh - uye] * [uxh - uxe; uyh - uye] * Quad.w(ip,1) * Jdet;
+       eL2u_den = eL2u_den + [uxe, uye] * [uxe; uye] * Quad.w(ip,1) * Jdet;
        
        % L2 norm pressure
-       eL2p_num = eL2p_num + [uxh_p - uxe_p, uyh_p - uye_p] * [uxh_p - uxe_p; uyh_p - uye_p] * Quad.w(ip,1) * Jdet;
-       eL2p_den = eL2p_den + [uxe_p, uye_p] * [uxe_p; uye_p] * Quad.w(ip,1) * Jdet;
+       eL2p_num = eL2p_num + [pxh - pxe, pyh - pye] * [pxh - pxe; pyh - pye] * Quad.w(ip,1) * Jdet;
+       eL2p_den = eL2p_den + [pxe, pye] * [pxe; pye] * Quad.w(ip,1) * Jdet;
+       
+       % approximate (FEM)
+       eh = Nu*e(connu_e);
+       sh = Nu*s(connu_e);
+       qh = Np*q(connp_e);
+       
+       % exact
+       ee = Nu*e_exact(connu_e);
+       se = Nu*s_exact(connu_e);
+       qe = Np*q_exact(connp_e);
+       
+%        ee = eval(subs(e_exact, Nu*MeshU.coords(connu_e)));
+%        se = eval(subs(s_exact, Nu*MeshU.coords(connu_e)));
+
+%        qe = eval(subs(qex_exact, Np*MeshP.coords(connp_e)));
+     
+       % energy norm
+       eENu_num = eENu_num + (eh-ee) * (sh-se) * Quad.w(ip,1) * Jdet;
+       eENu_den = eENu_den + ee * se * Quad.w(ip,1) * Jdet;
+       eENp_num = eENp_num + (qh-qe) * (qh-qe) * Quad.w(ip,1) * Jdet;
+       eENp_den = eENp_den + qe * qe * Quad.w(ip,1) * Jdet;
+
+
+       % H1 norm
+       eH1u_num = eH1u_num + ((eh-ee)^2 + (uxh - uxe)^2) * Quad.w(ip,1) * Jdet;
+       eH1u_den = eH1u_den + ((ee)^2 + (uxe)^2) * Quad.w(ip,1) * Jdet;
+       eH1p_num = eH1p_num + ((qh-qe)^2 + (pxh - pxe)^2) * Quad.w(ip,1) * Jdet;
+       eH1p_den = eH1p_den + ((qe)^2 + (pxe)^2) * Quad.w(ip,1) * Jdet;
     end
 end
 
 % square root
 ErrorComp.eL2u = sqrt(eL2u_num/eL2u_den);
 ErrorComp.eL2p = sqrt(eL2p_num/eL2p_den);
+
+ErrorComp.eENp = sqrt(eENp_num/eENp_den);    
+ErrorComp.eENu = sqrt(eENu_num/eENu_den);
+
+ErrorComp.eH1p = sqrt(eH1p_num/eH1p_den);    
+ErrorComp.eH1u = sqrt(eH1u_num/eH1u_den);
 
 end
--- Functions/Post Processing/PlotConvergenceRate.m
+++ Functions/Post Processing/PlotConvergenceRate.m
@@ -9,78 +9,164 @@ clear, clc
 close all
 
 %% Initialize variables
-nsims = 4;
+nsims = 5;
 h = zeros(nsims,1);
 eL2u = zeros(nsims,1);
 eL2p = zeros(nsims,1);
+eENu = zeros(nsims,1);
+eENp = zeros(nsims,1);
+eH1u = zeros(nsims,1);
+eH1p = zeros(nsims,1);
 
 %% Mesh 1
 % load file
-load Results_m1_space_Boone.mat
+load Results_m1Boone_testH1.mat
 % mesh size
 h(1) = ErrorComp.h;
 % error displacement
 eL2u(1) = ErrorComp.eL2u;
 % error pressure
 eL2p(1) = ErrorComp.eL2p;
+% error strain
+eENu(1) = ErrorComp.eENu;
+% error flux
+eENp(1) = ErrorComp.eENp;
+% error H1
+eH1u(1) = ErrorComp.eH1u;
+% error H1
+eH1p(1) = ErrorComp.eH1p;
 
 %% Mesh 2
-load Results_m2_space_Boone.mat
+load Results_m2Boone_testH1.mat
 % mesh size
 h(2) = ErrorComp.h;
 % error displacement
 eL2u(2) = ErrorComp.eL2u;
 % error pressure
 eL2p(2) = ErrorComp.eL2p;
+% error strain
+eENu(2) = ErrorComp.eENu;
+% error flux
+eENp(2) = ErrorComp.eENp;
+% error H1
+eH1u(2) = ErrorComp.eH1u;
+% error H1
+eH1p(2) = ErrorComp.eH1p;
 
 %% Mesh 3
-load Results_m3_space_Boone.mat
+load Results_m3Boone_testH1.mat
 % mesh size
 h(3) = ErrorComp.h;
 % error displacement
 eL2u(3) = ErrorComp.eL2u;
 % error pressure
 eL2p(3) = ErrorComp.eL2p;
+% error strain
+eENu(3) = ErrorComp.eENu;
+% error flux
+eENp(3) = ErrorComp.eENp;
+% error H1
+eH1u(3) = ErrorComp.eH1u;
+% error H1
+eH1p(3) = ErrorComp.eH1p;
 
 %% Mesh 4
-load Results_m4_space_Boone.mat
+load Results_m4Boone_testH1.mat
 % mesh size
 h(4) = ErrorComp.h;
 % error displacement
 eL2u(4) = ErrorComp.eL2u;
 % error pressure
 eL2p(4) = ErrorComp.eL2p;
+% error strain
+eENu(4) = ErrorComp.eENu;
+% error flux
+eENp(4) = ErrorComp.eENp;
+% error H1
+eH1u(4) = ErrorComp.eH1u;
+% error H1
+eH1p(4) = ErrorComp.eH1p;
 
 %% Mesh 5
-% load Results_m5_space_Boone.mat
-% % mesh size
-% h(5) = ErrorComp.h;
-% % error displacement
-% eL2u(5) = ErrorComp.eL2u;
-% % error pressure
-% eL2p(5) = ErrorComp.eL2p;
+load Results_m5Boone_testH1.mat
+% mesh size
+h(5) = ErrorComp.h;
+% error displacement
+eL2u(5) = ErrorComp.eL2u;
+% error pressure
+eL2p(5) = ErrorComp.eL2p;
+% error strain
+eENu(5) = ErrorComp.eENu;
+% error flux
+eENp(5) = ErrorComp.eENp;
+% error H1
+eH1u(5) = ErrorComp.eH1u;
+% error H1
+eH1p(5) = ErrorComp.eH1p;
 
 %% Curve slope
 % Determine slope of L2 norm
-pL2u = polyfit(log(h), log(eL2u),1);
+pL2u = polyfit(log(h(1:2)), log(eL2u(1:2)),1);
 m_L2u = pL2u(1);
 
-pL2p = polyfit(log(h), log(eL2p),1);
+pL2p = polyfit(log(h(1:2)), log(eL2p(1:2)),1);
 m_L2p = pL2p(1);
 
+% Determine slope of energy norm
+pENu = polyfit(log(h(1:2)), log(eENu(1:2)),1);
+m_ENu = pENu(1);
+
+pENp = polyfit(log(h(1:2)), log(eENp(1:2)),1);
+m_ENp = pENp(1);
+
+% Determine slope of H1 norm
+pH1u = polyfit(log(h(1:2)), log(eH1u(1:2)),1);
+m_H1u = pH1u(1);
+
+pH1p = polyfit(log(h(1:2)), log(eH1p(1:2)),1);
+m_H1p = pH1p(1);
+
 %% Plots
-% displacement
+% L2 norm displacement
 figure;
 loglog(h,eL2u,'g-o', 'LineWidth', 1.5);
 xlabel('Mesh size (m)');
 ylabel('L2-norm');
-title(sprintf('Convergence for displacement order %.2f', m_L2u));
+title(sprintf('L2 - u Convergence: %.2f', m_L2u));
 
-% pressure
+% L2 norm pressure
 figure;
 loglog(h,eL2p,'m-o', 'LineWidth', 1.5);
 xlabel('Mesh size (m)');
 ylabel('L2-norm');
-title(sprintf('Convergence for pressure order %.2f', m_L2p));
+title(sprintf('L2 - p Convergence: %.2f', m_L2p));
+
+% Energy norm displacement
+figure;
+loglog(h,eENu,'b-o', 'LineWidth', 1.5);
+xlabel('Mesh size (m)');
+ylabel('e-norm');
+title(sprintf('Energy - u Convergence: %.2f', m_ENu));
+
+% Energy norm pressure
+figure;
+loglog(h,eENp,'k-o', 'LineWidth', 1.5);
+xlabel('Mesh size (m)');
+ylabel('q-norm');
+title(sprintf('Energy - p Convergence: %.2f', m_ENp));
+
+% H1 norm displacement
+figure;
+loglog(h,eH1u,'r-o', 'LineWidth', 1.5);
+xlabel('Mesh size (m)');
+ylabel('e-norm');
+title(sprintf('H1 - u Convergence: %.2f', m_H1u));
+
+% H1 norm norm pressure
+figure;
+loglog(h,eH1p,'c-o', 'LineWidth', 1.5);
+xlabel('Mesh size (m)');
+ylabel('q-norm');
+title(sprintf('H1 - p Convergence: %.2f', m_H1p));
 
 end
