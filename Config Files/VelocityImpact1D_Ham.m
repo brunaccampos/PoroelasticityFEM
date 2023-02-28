25a9545ffd886a7d26b26aec3656279e2bebafde
Bruna <bccampos@uwaterloo.ca>
2023-02-28 15:07:00 -0500

    Add new elastic bar impact problem

---
+++ Config Files/VelocityImpact1D_Ham.m
@@ -0,0 +1,195 @@
+function [Material, MeshU, MeshP, MeshN, BC, Control] = VelocityImpact1D_Idesman(config_dir, progress_on)
+% Column Consolidation 2D simulation
+% Configuration File
+% Based on Zienkiewicz (1982) model
+%
+% Assumptions/conventions:
+% - stress is positive for tension
+% - boundary condition for force is based on total stress
+% - only solid acceleration is considered (undrained condition; no motions
+% of the fluid relative to the solid skeleton can occur)
+% - solid grains and fluid are incompressible
+
+%% Poroelasticity model
+% 1 - Biot theory
+% 0 - Spanos theory (additional porosity equation)
+Control.Biotmodel = 1;
+
+%% Material properties - Idesman (2011)
+% elasticity modulus [Pa]
+Material.E = 200e9;
+% average density of the medium
+Material.rho = 8000;
+% Poisson's ratio
+Material.nu = 0.0;
+
+% porous media permeability [m2/GPa s]
+Material.kf = 0;
+% Biot's coefficient
+Material.alpha = 0;
+% 1/Q (related to storage coefficient)
+Material.Minv = 0;
+% fluid density [10^9 kg/m3]
+Material.rho_f = 0;
+
+% lumped mass matrix - 0: false, 1: true
+Material.lumpedMass = 1;
+
+% thickness 
+% 1D: cross sectional area [m2]
+% 2D: out of plane thickness [m]
+Material.t = 0.01;
+
+% constititive law - 'PlaneStress' or 'PlaneStrain'
+% Note: use 'PlaneStrain' for 1D or 2D poroelasticity
+Material.constLaw = 'PlaneStrain';
+
+%% Mesh parameters
+if progress_on
+    disp([num2str(toc),': Building Mesh...']);
+end
+
+% mesh type
+% 'Manual': 1D mesh
+% 'Gmsh': 2D mesh, input file from GMSH
+MeshType = 'Manual';
+
+switch MeshType
+    case 'Manual'
+        % number of space dimensions
+        nsd = 1;
+        % number of elements
+        ne = 100;
+        % column size [m]
+        L = 0.5;
+        %%%% solid displacement field
+        typeU = 'L2';
+        fieldU = 'u';
+        MeshU = Build1DMesh(nsd, ne, L, typeU, fieldU);
+        %%%% fluid pressure field
+        typeP = 'L2';
+        fieldP = 'p';
+        MeshP = Build1DMesh(nsd, ne, L, typeP, fieldP);
+        %%%% porosity field
+        if ~Control.Biotmodel
+            typeN = 'L2';
+            fieldN = 'n';
+            MeshN = Build1DMesh(nsd, ne, L, typeN, fieldN);
+        else
+            MeshN = [];
+        end
+    case 'Gmsh'
+        % Version 2 ASCII
+        % number of space dimensions
+        nsd = 2;
+        %%%% displacement field
+        fieldU = 'u';
+        meshFileNameU = 'Mesh Files\VelocityImpactQ9.msh';
+        MeshU = BuildMesh_GMSH(meshFileNameU, fieldU, nsd, config_dir, progress_on);
+        %%%% pressure field
+        fieldP = 'p';
+        meshFileNameP = 'Mesh Files\VelocityImpactQ4.msh';
+        MeshP = BuildMesh_GMSH(meshFileNameP, fieldP, nsd, config_dir, progress_on);
+        %%%% porosity field
+        if ~Control.Biotmodel
+            fieldN = 'n';
+            meshFileNameN = 'Mesh Files\VelocityImpactQ4.msh';
+            MeshN = BuildMesh_GMSH(meshFileNameN, fieldN, nsd, config_dir, progress_on);
+        else
+            MeshN = [];
+        end
+end
+
+%% Initial conditions
+% displacement
+BC.initU = [];
+
+% pressure
+BC.initP = [];
+
+%% Find nodes for prescribed BCs
+% find top and bottom nodes for displacement field
+BC.top_node_u = find(MeshU.coords == max(MeshU.coords));
+BC.bottom_node_u = find(MeshU.coords == min(MeshU.coords));
+
+% find top and bottom nodes for pressure field
+BC.top_node_p = find(MeshP.coords == max(MeshP.coords));
+BC.bottom_node_p = find(MeshP.coords == min(MeshP.coords));
+
+%% Dirichlet BCs - solid
+% displacement u=0 at bottom and top
+BC.fixed_u1 = BC.top_node_u;
+% u = t at left (x)
+BC.fixed_u2 = BC.bottom_node_u;
+% fixed DOFs
+BC.fixed_u = [BC.fixed_u1; BC.fixed_u2];
+% fixed DOF values
+BC.fixed_u_value1 = zeros(length(BC.fixed_u1),1);
+BC.fixed_u_value2 = ones(length(BC.fixed_u2),1);
+BC.fixed_u_value = [BC.fixed_u_value1; BC.fixed_u_value2];
+% free nodes
+BC.free_u = setdiff(MeshU.DOF, BC.fixed_u);
+
+%% Dirichlet BCs - fluid
+% pressure p=0 at all boundaries
+BC.fixed_p = 1:MeshP.nDOF;
+% fixed DOF values
+BC.fixed_p_value = zeros(length(BC.fixed_p),1);
+% free nodes
+BC.free_p = setdiff(MeshP.DOF, BC.fixed_p);
+
+%% Neumann BCs - solid
+% prescribed traction [GN/m2]
+BC.tractionNodes = [];
+
+% point loads [GN]
+BC.pointLoad = [];
+
+% body force [GN/m3]
+BC.b = @(x)[];  
+
+%% Neumann BCs - fluid
+% distributed flux [m3/s]
+% impervious at bottom, left, and right
+BC.fluxNodes = [];
+
+% point flux [m/s]
+BC.pointFlux = [];
+
+% flux source [m3/s/m3]
+BC.s = @(x)[]; 
+
+%% Quadrature order
+Control.nqU = 3;
+Control.nqP = 3;
+
+%% Problem type
+% 1 = quasi-steady/transient problem (no acceleration and pressure change)
+% 0 = dynamic problem (acceleration/intertia terms included)
+Control.steady = 0;
+
+% tag used for computing analytical solution
+% 1 = uncoupled problem (elasticity, heat transfer, etc)
+% 0 = coupled problem (Biot, Spanos model)
+Control.uncoupled = 0; 
+
+%% Solution parameters
+Control.dt = 2.5e-8;  % time step
+Control.tend = 0.00005;   % final simulation time
+
+Control.plotu = find(MeshU.coords == max(MeshU.coords)/2); % middle node
+Control.plotp = find(MeshU.coords == max(MeshU.coords)/2); % middle node
+
+% plot analytical solution (valid for 1D problems with Material.Minv == 0)
+Control.plotansol = 0; % 1 = true; 0 = false
+
+% solve in the frequency domain
+Control.freqDomain = 0;  % 1 = true; 0 = false
+
+%% Time discretization parameters
+% Newmark method
+Control.beta = 0.25;
+Control.gamma = 0.5;
+Control.theta = 0.5;
+
+end
