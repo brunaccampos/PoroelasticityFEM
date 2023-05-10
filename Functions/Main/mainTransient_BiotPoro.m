e86ecd869a7bcfda8f73d91d588cefd351726ec5
Bruna <bccampos@uwaterloo.ca>
2023-05-10 13:10:00 -0400

    Add functions for transient Biot model u-p with porosity variation

---
+++ Functions/Main/mainTransient_BiotPoro.m
@@ -0,0 +1,138 @@
+% Biot poroelasticity model
+% Transient case
+% December 2022
+% ------------------------------------------------------------------------
+% May 2023: version with u-p system, substituting porosity equation
+% ------------------------------------------------------------------------
+
+%% Model name and type
+disp([num2str(toc),': Model: Biot transient case, implicit porosity variation']);
+
+%% Assemble system matrices
+disp([num2str(toc),': Assembling System Matrices...']);
+
+[Kuu, Kup, Kpp, Kpu, S] = ComputeMatricesTransient_BiotPoro(Material, MeshU, MeshP, QuadU, QuadP);
+
+%% Solve eigenproblem
+if Control.freqDomain
+    disp([num2str(toc),': Solving Uncoupled Eigenproblems...']);
+    [phi_u, omega2_u, phi_p, omega2_p] = SolveEigTransient_Biot(Kuu, Kup, Kpp, S, MeshU, MeshP, BC, Control);
+else
+    phi_u = [];
+    phi_p = [];
+end
+
+%% Initialize iteration variables
+[Iteration, Plot] = initVariables(phi_u, phi_p, [], MeshU, MeshP, MeshN, Material, Control, BC);
+
+%% Initial condition file
+if plot2vtk
+    % time domain solution
+    Solution.u = Iteration.u_old;
+    Solution.udot = Iteration.udot_old;
+    Solution.p = Iteration.p_old;
+
+    % save vtk file
+    PostProcessing(Solution, Material, MeshU, MeshP, MeshN, Control, BC, config_name, vtk_dir);
+
+    % update time step
+    Control.step = 1;
+end
+
+%% Solve system
+for t = 0:Control.dt:Control.tend
+    fprintf('\n Step %d, t = %d \n', Control.step, t);
+    Control.t = t;
+
+    % Assemble system load vectors
+    [fu,fp,fn] = ComputeLoads(BC, MeshU, MeshP, MeshN, Control, QuadU, QuadP);
+    
+    % analytical solution for 1D case
+    if Control.plotansol
+        if Control.uncoupled
+            p_an = Control.p_an;
+            u_an = Control.u_an;
+        else
+            if any(Material.Minv)
+                [p_an, u_an] = getAnalyticResult_Comp(Material, MeshU, MeshP, BC, Control);
+            else % 1/M = 0
+                [~, p_an, u_an] = getAnalyticResult_Incomp(Material, MeshU, MeshP, BC, Control);
+            end
+        end
+       
+        % store variables over length
+        Plot.pan_space = p_an;
+        Plot.uan_space = u_an;
+
+        % store variables over time
+        if Control.step < length(Plot.time)
+            Plot.uan_time(Control.step+1,:) = u_an(Control.plotu, 1);
+            Plot.pan_time(Control.step+1,:) = p_an(Control.plotp, 1);
+        end
+    end
+
+    % linear solver
+    [Solution] = SolverTransient_Biot(Kuu, Kup, Kpp, Kpu, S, fu, fp, BC, Control, Iteration);
+    
+    % solution in the frequency domain
+    if Control.freqDomain
+        [SolutionFreq] = SolverTransientFreq_Biot(phi_u, phi_p, Kuu, Kup, Kpp, S, fu, fp, BC, Control, Iteration);
+    end
+
+    % update external forces vectors
+    fu(BC.fixed_u) = Solution.fE;
+    fp(BC.fixed_p) = Solution.qE;
+
+    % post processing: compute stress/flux, export VTK file
+    if plot2vtk
+        PostProcessing(Solution, Material, MeshU, MeshP, MeshN, Control, BC, config_name, vtk_dir);
+    end
+
+    % store variables over time
+    if Control.step < length(Plot.time)
+        % time domain
+        % plot pressure vs time
+        Plot.p_time(Control.step+1,:) = Solution.p(Control.plotp, 1);
+        % plot displacement vs time
+        Plot.u_time(Control.step+1,:) = Solution.u(Control.plotu, 1);
+        % plot velocity vs time
+        Plot.udot_time(Control.step+1,:) = Solution.udot(Control.plotu, 1);
+
+        % frequency domain
+        if Control.freqDomain
+            % plot pressure vs time
+            Plot.pF(Control.step+1,:) = SolutionFreq.pF(Control.plotp, 1);
+            % plot displacement vs time
+            Plot.uF(Control.step+1,:) = SolutionFreq.uF(Control.plotu, 1);
+            % plot velocity vs time
+            Plot.uFdot(Control.step+1,:) = SolutionFreq.uFdot(Control.plotu, 1);
+        end
+    end
+
+    % store variables over space
+    Plot.p_space = Solution.p(Control.plotp);
+    Plot.u_space = Solution.u(Control.plotu);
+    Plot.udot_space = Solution.udot(Control.plotu);
+
+    % update variables - time domain
+    Iteration.u_old = Solution.u; % solid displacement
+    Iteration.udot_old = Solution.udot; % solid velocity
+    Iteration.p_old = Solution.p; % fluid pressure
+    Iteration.fu_old = fu; % load vector
+    Iteration.fp_old = fp; % flux vector
+
+    % update variables - frequency domain
+    if Control.freqDomain
+        Iteration.xuF_old = SolutionFreq.xuF;
+        Iteration.xuFdot_old = SolutionFreq.xuFdot;
+        Iteration.xpF_old = SolutionFreq.xpF;
+
+        Iteration.uF_old = SolutionFreq.uF; % solid displacement
+        Iteration.uFdot_old = SolutionFreq.uFdot; % solid velocity
+        Iteration.pF_old = SolutionFreq.pF; % fluid pressure
+    end
+
+    % update time and step
+    Control.step = Control.step + 1;
+end
+
