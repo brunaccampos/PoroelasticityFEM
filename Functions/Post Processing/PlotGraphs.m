627f4a991da37d6ff81b0b646c5d040dd67a2f9c
Bruna <bccampos@uwaterloo.ca>
2023-05-17 11:51:09 -0400

    Update plots for pressure

--- Functions/Post Processing/PlotGraphs.m
+++ Functions/Post Processing/PlotGraphs.m
@@ -33,19 +33,19 @@ end
 function plot1Dsteady(Solution, SolutionFreq, Material, MeshU, MeshP, MeshN, Control, Plot, saveGraphs_on)
 %% pressure vs depth
 figure;
-plot(MeshP.coords, Solution.p*10^9,'k','LineWidth',2);
+plot(MeshP.coords, Solution.p*10^9.*Material.t,'k','LineWidth',2);
 hold on
 xlabel('Column depth [m]');
 ylabel('p [Pa]');
 title(sprintf('Pressure at t = %.0f s', Control.tend));
 % frequency domain solution
 if Control.freqDomain
-    plot(MeshP.coords, SolutionFreq.pF*10^9,'r--','LineWidth',2);
+    plot(MeshP.coords, SolutionFreq.pF*10^9.*Material.t,'r--','LineWidth',2);
     legend('Time', 'Frequency');
 end
 % analytical solution
 if Control.plotansol
-    plot(MeshP.coords, Plot.pan_space*10^9,'k:','LineWidth',2);
+    plot(MeshP.coords, Plot.pan_space*10^9.*Material.t,'k:','LineWidth',2);
     legend('Numerical', 'Analytical');
 end
 % adapt legend if both frequency and analytical solutions are plotted
@@ -58,19 +58,19 @@ if saveGraphs_on
 end
 %% pressure vs time
 figure;
-plot(Plot.time, Plot.p_time*10^9,'k','LineWidth',2);
+plot(Plot.time, Plot.p_time*10^9.*Material.t,'k','LineWidth',2);
 hold on
 xlabel('Time [s]');
 ylabel('p [Pa]');
 title(sprintf('Pressure at x = %.2f m', MeshP.coords(Control.plotp,1)));
 % frequency domain solution
 if Control.freqDomain
-    plot(Plot.time, Plot.pF*10^9,'r--','LineWidth',2);
+    plot(Plot.time, Plot.pF*10^9.*Material.t,'r--','LineWidth',2);
     legend('Time', 'Frequency');
 end
 % analytical solution
 if Control.plotansol
-    plot(Plot.time, Plot.pan_time*10^9,'k:','LineWidth',2);
+    plot(Plot.time, Plot.pan_time*10^9.*Material.t,'k:','LineWidth',2);
     legend('Numerical', 'Analytical');
 end
 % adapt legend if both frequency and analytical solutions are plotted
