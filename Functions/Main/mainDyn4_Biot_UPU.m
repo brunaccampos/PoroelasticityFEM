482f8a79f305644a90a5927f18b0c2a57ab396f1
Bruna <bccampos@uwaterloo.ca>
2023-06-20 08:06:00 -0400

    Add plot options for fixed depth in 2D

--- Functions/Main/mainDyn4_Biot_UPU.m
+++ Functions/Main/mainDyn4_Biot_UPU.m
@@ -156,3 +156,11 @@ while Control.t < Control.tend
     Control.t = Control.t + Control.dtc;
 end
  
+% plot variables in x for fixed y (2D case)
+if isfield(Control, 'depthplot')
+    Plot.urow = Solution.u(Control.ploturow);
+    Plot.udotrow = Solution.udot(Control.ploturow);
+    Plot.prow = Solution.p(Control.plotprow);
+    Plot.ufrow = Solution.uf(Control.ploturow);
+    Plot.ufdotrow = Solution.ufdot(Control.ploturow);
+end
