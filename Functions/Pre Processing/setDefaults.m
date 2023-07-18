dbbb67f37a0b3316b31e9089eda1c2e08014a71f
Bruna <bccampos@uwaterloo.ca>
2023-07-18 10:11:46 -0400

    Update set defaults with added mass coefficient

--- Functions/Pre Processing/setDefaults.m
+++ Functions/Pre Processing/setDefaults.m
@@ -2,28 +2,39 @@ function [Material, BC, Control] = setDefaults(Material, BC, Control)
 % Set defaults
 
 %% Material model
+% constitutive law: default plane stress
 if ~isfield(Material, 'constLaw')
     Material.constLaw = 'PlaneStress';
 end
 
+% lumped mass matrix: default false
 if ~isfield(Material, 'lumpedMass')
     Material.lumpedMass = 0;
 end
 
+% lumped damping matrix: default false
 if ~isfield(Material, 'lumpedDamping')
     Material.lumpedDamping = 0;
 end
 
+% added mass coefficient: default zero
+if ~isfield(Material, 'rho12')
+    Material.rho12 = 0;
+end
+
 %% Boundary/Initial conditions
+% initial condition for solid displacement field
 if ~isfield(BC, 'initU')
     BC.initU = [];
 end
 
+% initial condition fluid pressure field
 if ~isfield(BC, 'initP')
     BC.initP = [];
 end
 
 %% Control options
+% ramp applied load in the beginning of simulation
 if ~isfield(Control, 'rampLoad')
    Control.rampLoad = 0; 
 end
