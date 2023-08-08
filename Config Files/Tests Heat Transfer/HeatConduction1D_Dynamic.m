a9ef954a8182a8a8e85083bb0da03c384ab5cc82
Bruna <bccampos@uwaterloo.ca>
2023-08-08 08:25:07 -0400

    Organize config files

--- Config Files/Tests Heat Transfer/HeatConduction1D_Dynamic.m
+++ Config Files/Tests Heat Transfer/HeatConduction1D_Dynamic.m
@@ -1,5 +1,5 @@
 function [Material, MeshU, MeshP, MeshN, BC, Control] = HeatConduction1D_Dynamic(config_dir, progress_on)
-% 
+% Heat conduction in 1D
 % Configuration File
 % ------------------------------------------------------------------------
 
@@ -9,7 +9,7 @@ Material.kf = 1;
 % 1/Q (related to storage coefficient)
 Material.Minv = 1;
 % Poroelasticity model
-Control.PMmodel = 'Dy1_Biot_UP';
+Control.PMmodel = 'Dyn1_Biot_UP';
 
 % material density [kg/m3]
 Material.rho = 0;
@@ -27,7 +27,7 @@ Control.Biotmodel = 1;
 % lumped mass matrix - 0: false, 1: true
 Material.lumpedMass = 0;
 
-% thickness 
+% thickness
 % 1D: cross sectional area [m2]
 % 2D: out of plane thickness [m]
 Material.t = 1;
@@ -41,79 +41,53 @@ if progress_on
     disp([num2str(toc),': Building Mesh...']);
 end
 
-% mesh type
-% 'Manual': 1D mesh
-% 'Gmsh': 2D mesh, input file from GMSH
-MeshType = 'Manual';
+% location of initial node [m] [x0;y0;z0]
+coord0 = [0;0;0];
+% number of space dimensions
+nsd = 1;
+% size of domain [m] [Lx;Ly;Lz]
+L = 20;
+% number of elements in each direction [nex; ney; nez]
+ne = 10;
 
-switch MeshType
-    case 'Manual'
-        % number of space dimensions
-        nsd = 1;
-        % number of elements
-        ne = 10;
-        % column size [m]
-        L = 20;
-        %%%% solid displacement field
-        typeU = 'L3';
-        fieldU = 'u';
-        MeshU = Build1DMesh(nsd, ne, L, typeU, fieldU);
-        %%%% fluid pressure field
-        typeP = 'L2';
-        fieldP = 'p';
-        MeshP = Build1DMesh(nsd, ne, L, typeP, fieldP);
-        %%%% porosity field
-        if contains(Control.PMmodel, 'UPN')
-            typeN = 'L2';
-            fieldN = 'p';
-            MeshN = Build1DMesh(nsd, ne, L, typeN, fieldN);
-        else
-            MeshN = [];
-        end
-    case 'Gmsh'
-        % Version 2 ASCII
-        % number of space dimensions
-        nsd = 2;
-        %%%% displacement field
-        fieldU = 'u';
-        meshFileNameU = 'Column2DQ9.msh';
-        MeshU = BuildMesh_GMSH(meshFileNameU, fieldU, nsd, config_dir, progress_on);
-        %%%% pressure field
-        fieldP = 'p';
-        meshFileNameP = 'Column2DQ4.msh';
-        MeshP = BuildMesh_GMSH(meshFileNameP, fieldP, nsd, config_dir, progress_on);
-        %%%% porosity field
-        if contains(Control.PMmodel, 'UPN')
-            fieldN = 'n';
-            meshFileNameN = 'Column2DQ4.msh';
-            MeshN = BuildMesh_GMSH(meshFileNameN, fieldN, nsd, config_dir, progress_on);
-        else
-            MeshN = [];
-        end
+%%%% displacement mesh
+% element type ('Q4')
+typeU = 'L3';
+% variable field ('u', 'p', 'n')
+fieldU = 'u';
+MeshU = BuildMesh_structured(nsd, coord0, L, ne, typeU, fieldU, progress_on);
+
+%%%% pressure mesh
+% element type ('Q4')
+typeP = 'L2';
+% variable field ('u', 'p', 'n')
+fieldP = 'p';
+MeshP = BuildMesh_structured(nsd, coord0, L, ne, typeP, fieldP, progress_on);
+
+%%%% porosity mesh
+if contains(Control.PMmodel, 'UPN')
+    % element type ('Q4')
+    typeN = 'L2';
+    % variable field ('u', 'p', 'n')
+    fieldN = 'n';
+    MeshN = BuildMesh_structured(nsd, coord0, L, ne, typeN, fieldN, progress_on);
+else
+    MeshN = [];
 end
-
-%% Find nodes for prescribed BCs
-% find top and bottom nodes for displacement field
-BC.top_node_u = find(MeshU.coords == max(MeshU.coords));
-BC.bottom_node_u = find(MeshU.coords == min(MeshU.coords));
-
-% find top and bottom nodes for pressure field
-BC.top_node_p = find(MeshP.coords == max(MeshP.coords));
-BC.bottom_node_p = find(MeshP.coords == min(MeshP.coords));
 
 %% Dirichlet BCs - solid
 % column vector of prescribed displacement dof
 BC.fixed_u = 1:MeshU.nDOF;
 % prescribed displacement for each dof [u1; u2; ...] [m]
-BC.fixed_u_value = zeros(length(BC.fixed_u),1);
+BC.fixed_u_value = @(t) zeros(length(BC.fixed_u),1);
 % free nodes
 BC.free_u = setdiff(MeshU.DOF, BC.fixed_u);
 
 %% Dirichlet BCs - fluid
-%   pressure p=0 at the top
-BC.fixed_p = BC.top_node_p;
+% pressure p=0 at the top
+BC.fixed_p = MeshP.left_nodes;
 % fixed DOF values
-BC.fixed_p_value = zeros(length(BC.fixed_p),1);
+BC.fixed_p_value = @(t) zeros(length(BC.fixed_p),1);
 % free nodes
 BC.free_p = setdiff(MeshP.DOF, BC.fixed_p);
 
@@ -126,12 +100,12 @@ BC.tractionForce = zeros(length(BC.tractionNodes),1);
 BC.pointLoad = [];
 
 % body force [N/m3]
-BC.b = @(x)[];  
+BC.b = @(x,t)[];
 
 %% Neumann BCs - fluid
 % point flux [m3/s]
 BC.pointFluxValue = -1;
-BC.pointFluxNodes = BC.bottom_node_p;
+BC.pointFluxNodes = MeshP.right_nodes;
 BC.pointFlux = zeros(MeshP.nDOF,1);
 BC.pointFlux(BC.pointFluxNodes) = BC.pointFluxValue;
 
@@ -139,7 +113,7 @@ BC.pointFlux(BC.pointFluxNodes) = BC.pointFluxValue;
 BC.fluxNodes = [];
 
 % flux source [m3/s/m3]
-BC.s = @(x)[]; 
+BC.s = @(x,t)[];
 
 %% Porosity BCs
 if contains(Control.PMmodel, 'UPN')
@@ -158,7 +132,7 @@ Control.freqDomain = 0;  % 1 = true; 0 = false
 %% Analytical solution
 % 1 = uncoupled problem (elasticity, heat transfer, etc)
 % 0 = coupled problem (Biot, Spanos model)
-Control.uncoupled = 0; 
+Control.uncoupled = 0;
 
 % plot analytical solution (valid for 1D problems with Material.Minv == 0)
 Control.plotansol = 0; % 1 = true; 0 = false
