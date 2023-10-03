35d9ac9f13fd0ca5b349d6f762b6fb5c12fcd218
Bruna <bccampos@uwaterloo.ca>
2023-10-03 12:23:24 -0400

    Fix vel/att coeffs for Biot model

--- Functions/Pre Processing/getWaveCoeffs_BT.m
+++ Functions/Pre Processing/getWaveCoeffs_BT.m
@@ -16,30 +16,38 @@ rhos = Material.rhos; % solid density
 mus = Material.mus; % solid shear modulus
 Ks = Material.Ks; % solid bulk modulus
 rho12 = Material.rho12; % coupled density
+deltas = Material.deltas; % compliance coefficient solid
+deltaf = Material.deltaf; % compliance coefficient fluid
 eta0 = Material.eta0; % porosity
 k = Material.k; % intrinsic permeability
 
 %% Auxiliar constants
-c2 = Kf/rhof;
 vps2 = (Ks+4*mus/3)/rhos;
 aps = muf*eta0^2/(k*rhos*(1-eta0));
-apf = (xif+4*muf/3)/rhof;
-bpf = muf*eta0/k/rhof;
 cps = rho12/(1-eta0)/rhos;
+dps = Ks*deltas/rhos/(1-eta0);
+hps = Ks*deltaf/rhos/(1-eta0);
+
+c2 = Kf/rhof;
+bpf = muf*eta0/k/rhof;
 cpf = rho12/eta0/rhof;
+dpf = Kf*deltas/rhof/eta0;
+hpf = Kf*deltaf/rhof/eta0;
+
 css = mus/rhos;
-csf = muf/rhof;
 dss = muf*eta0^2/(k*rhos*(1-eta0));
 dsf = muf*eta0/k/rhof;
 
+
 %% Compressional P wave
-Ap_BT = c2*vps2-1i*apf*vps2*w;
-Bp_BT = -1i*(c2*aps+bpf*vps2)*w-(vps2+c2-c2*cps+apf*aps-cpf*vps2)*w.^2+1i*(apf-apf*cps)*w.^3;
+Ap_BT = (c2*vps2-vps2*hpf-dps*c2+dps*hpf-hps*dpf)*ones(1,length(w));
+Bp_BT = (-c2*aps-bpf*vps2+dps*bpf+aps*hpf-hps*bpf-aps*dpf)*1i*w +...
+    (-vps2-c2+c2*cps+cpf*vps2+hpf+dps-dps*cpf-cps*hpf+hps*cpf+cps*dpf)*w.^2;
 Cp_BT = (1-cps-cpf)*w.^4+1i*(aps+bpf)*w.^3;
 
 %% Shear S wave
-As_BT = -1i*csf*css*w;
-Bs_BT = -(css+csf*dss-css*cpf)*w.^2+1i*(csf-csf*cps)*w.^3-1i*dsf*css*w;
+As_BT = zeros(1,length(w));
+Bs_BT = -(css-css*cpf)*w.^2-1i*dsf*css*w;
 Cs_BT = (1-cps-cpf)*w.^4+1i*(dss+dsf)*w.^3;
 
 end
