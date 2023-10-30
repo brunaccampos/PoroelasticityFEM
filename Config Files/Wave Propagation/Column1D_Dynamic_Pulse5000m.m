dfaaf2b4bd1807ee4299e6b2305cff805413ddd5
Bruna <bccampos@uwaterloo.ca>
2023-10-30 16:24:09 -0400

    Add config file 1D long domain

---
+++ Config Files/Wave Propagation/Column1D_Dynamic_Pulse5000m.m
@@ -0,0 +1,233 @@
+function [Material, MeshU, MeshP, MeshN, BC, Control] = Column1D_Dynamic_Pulse5000m(config_dir, progress_on)
+% Pulse propagation in 1D simulation
+% Configuration File
+% ------------------------------------------------------------------------
+% Based on Zienkiewicz (1982) model for dynamic case
+% ------------------------------------------------------------------------
+% Assumptions/conventions:
+% - stress is positive for tension
+% - boundary condition for force is based on total stress
+% ------------------------------------------------------------------------
+
+%% Poroelasticity model
+% Options:  Tr1_Biot_UP -------- Biot model (u-p), transient
+%           Tr2_Spanos_UPN ----- Spanos model (u-p-n), transient
+%           Tr3_Spanos_UP ------ Spanos model (u-p), dynamic, implicit
+%                                   porosity perturbation equation
+%           Dyn1_Biot_UP -------- Biot model (u-p), dynamic
+%           Dyn2_Spanos_UPN ----- Spanos model (u-p-n), dynamic
+%           Dyn3_Spanos_UP ------ Spanos model (u-p), dynamic, implicit
+%                                   porosity perturbation equation
+%           Dyn4_Biot_UPU ------- Biot model (u-p-U), dynamic
+%           Dyn5_Spanos_UPU ----- Spanos model (u-p-U), dynamic, implicit
+%                                   porosity perturbation equation
+Control.PMmodel = 'Dyn4_Biot_UPU';
+
+%% Material properties - Berea Sandstone (Detournay, 1993, p.26)
+% elasticity modulus [GPa]
+Material.E = 14.4;
+% Poisson's ratio
+Material.nu = 0.2;
+% intrinsic permeability [m2]
+Material.k = 1.88e-13;
+% dynamic viscosity [GPa s]
+Material.mu = 1e-12;
+% porous media permeability [m2/GPa s]
+Material.kf = Material.k/Material.mu;
+% Biot's coefficient
+Material.alpha = 0.79;
+% fluid bulk modulus [GPa]
+Material.Kf = 3.3;
+% solid bulk modulus [GPa]
+Material.Ks = 36;
+% material porosity
+Material.n = 0.19;
+% 1/Q (related to storage coefficient)
+Material.Minv = (Material.alpha - Material.n)/Material.Ks + Material.n/Material.Kf;
+% fluid bulk viscosity [GPa s]
+Material.xif = 2.8e-12; % (Quiroga-Goode, 2005)
+% fluid density [10^9 kg/m3]
+Material.rho_f = 1000e-9;
+% solid density [10^9 kg/m3]
+Material.rho_s = 2600e-9;
+% average density of the medium
+Material.rho = Material.n*Material.rho_f + (1-Material.n)*Material.rho_s;
+
+% thickness
+% 1D: cross sectional area [m2]
+% 2D: out of plane thickness [m]
+Material.t = 1;
+
+% constititive law - 'PlaneStress' or 'PlaneStrain'
+% Note: use 'PlaneStrain' for 1D or 2D poroelasticity
+Material.constLaw = 'PlaneStrain';
+
+% lumped mass matrix - 0: false, 1: true
+Material.lumpedMass = 0;
+
+% lumped damping matrix - 0: false, 1: true
+Material.lumpedDamping = 0;
+
+%% Spanos material parameters
+% porosity effective pressure coefficient (Spanos, 1989)
+% n = 0; % lower limit
+n = 1; % return to Biot
+% n = Material.Ks/Material.Kf; % upper limit
+
+% modified storage coefficient (Muller, 2019)
+Mstarinv = Material.Minv - (1-n)*(Material.alpha - Material.n)/Material.Ks;
+Mstar = 1/Mstarinv;
+
+% porosity equation coefficients
+Material.deltaF = (Material.alpha - Material.n) * Material.n * Mstar * n / Material.Ks;
+Material.deltaS = (Material.alpha - Material.n) * Material.n * Mstar / Material.Kf;
+
+%% Mesh parameters
+if progress_on
+    disp([num2str(toc),': Building Mesh...']);
+end
+
+% location of initial node [m] [x0;y0;z0]
+coord0 = [0;0;0];
+% number of space dimensions
+nsd = 1;
+% size of domain [m] [Lx;Ly;Lz]
+L = 5000;
+% number of elements in each direction [nex; ney; nez]
+ne = 10000;
+
+%%%% displacement mesh
+% element type ('Q4')
+typeU = 'L2';
+% variable field ('u', 'p', 'n')
+fieldU = 'u';
+MeshU = BuildMesh_structured(nsd, coord0, L, ne, typeU, fieldU, progress_on);
+
+%%%% pressure mesh
+% element type ('Q4')
+typeP = 'L2';
+% variable field ('u', 'p', 'n')
+fieldP = 'p';
+MeshP = BuildMesh_structured(nsd, coord0, L, ne, typeP, fieldP, progress_on);
+
+%%%% porosity mesh
+if contains(Control.PMmodel, 'UPN')
+    % element type ('Q4')
+    typeN = 'L2';
+    % variable field ('u', 'p', 'n')
+    fieldN = 'n';
+    MeshN = BuildMesh_structured(nsd, coord0, L, ne, typeN, fieldN, progress_on);
+else
+    MeshN = [];
+end
+
+%% Dirichlet BCs - solid
+% displacement at u=L
+BC.fixed_u1 = [MeshU.right_nodes; MeshU.left_nodes];
+% displacement at u=0 (sinusoidal)
+BC.fixed_u2 = find(MeshU.coords == 2500);
+BC.fixed_u = [BC.fixed_u1; BC.fixed_u2];
+% amplitude [GPa]
+P0 = 100e-2;
+% frequency [Hz]
+f = 1e1;
+BC.fixed_u_value = @(t) [0; 0; P0*(sin(pi*(t)*f) - 0.5*sin(2*pi*(t)*f))].*(t<1/f);
+% BC.fixed_u_value = @(t) [0; 0; P0*(sin(2*pi*(t)*f) - 0.5*sin(4*pi*(t)*f))].*(t<1/f);
+% free displacement nodes
+BC.free_u = setdiff(MeshU.DOF, BC.fixed_u);
+
+%% Dirichlet BCs - fluid displacement
+% displacement prescribed on the left and right
+BC.fixed_uf = [];
+BC.fixed_uf_value = @(t) zeros(length(BC.fixed_uf),1);
+% free displacement nodes
+BC.free_uf = setdiff(MeshU.DOF, BC.fixed_uf);
+
+%% Dirichlet BCs - fluid
+BC.fixed_p = [MeshP.left_nodes; MeshP.right_nodes];
+BC.fixed_p_value = @(t) zeros(length(BC.fixed_p),1);
+% free pressure nodes
+BC.free_p = setdiff(MeshP.DOF, BC.fixed_p);
+
+%% Neumann BCs - solid
+% point load [GN]
+BC.pointLoad = [];
+
+% distributed load [GN/m2]
+BC.tractionNodes = [];
+
+% body force [GN/m3]
+BC.b = @(x,t)[];
+
+%% Neumann BCs - fluid
+% point flux [m/s]
+BC.pointFlux = [];
+
+% distributed flux [m3/s]
+BC.fluxNodes = [];
+
+% flux source [m3/s/m3]
+BC.s = @(x,t)[];
+
+%% Porosity BCs
+if contains(Control.PMmodel, 'UPN')
+    BC.fixed_n = [];
+    BC.free_n = setdiff(MeshN.DOF, BC.fixed_n);
+    BC.fixed_n_value = zeros(length(BC.fixed_n),1);
+end
+
+%% Quadrature order
+Control.nqU = 3;
+Control.nqP = 3;
+
+%% Frequency domain
+Control.freqDomain = 0;  % 1 = true; 0 = false
+% plot f/fc range for frequency dependent BC
+range = 2; % 1: seismic, 2: acoustic, 3: ultrasonic
+% PlotFreqRange(Material, f, range);
+
+%% Analytical solution
+% 1 = uncoupled problem (elasticity, heat transfer, etc)
+% 0 = coupled problem (Biot, Spanos model)
+Control.uncoupled = 0;
+
+% plot analytical solution (valid for 1D problems with Material.Minv == 0)
+Control.plotansol = 0; % 1 = true; 0 = false
+
+%% Time step controls
+Control.dt = 1e-3;  % time step
+Control.tend = 6e-1;   % final simulation time
+
+% Newmark method
+Control.beta = 0.7;
+Control.gamma = 0.7;
+Control.theta = 0.7;
+Control.lambda = 0.7;
+
+% HHT method (-1/3 < alpha < 0)
+Control.alpha = 0;
+
+% adaptive time step (optional)
+% Control.dtmin = 1e-3; % minimum time step
+% Control.tlim = 1; % limit to use dtmin
+
+% ramp load option (optional); uses tlim from adaptive time step
+% NOTE: only declare if true
+% Control.rampLoad = 1;
+
+%% Plot data
+% DOF to plot graphs
+nodeU = find(MeshU.coords == 2500); % x = 2500m
+nodeP = find(MeshP.coords == 2500); % x = 2500m
+Control.plotu = nodeU;
+Control.plotp = nodeP;
+Control.depthDir = 1; % 1 = fixed y, vary x --- 2 = fixed x, vary y
+
+% Plot in a row
+Control.fixedDepthPlotON = 1; % 0: false, 1: true
+
+% Nodes to plot in a row (all nodes for 1D case)
+Control.ploturow = MeshU.DOF;
+Control.plotprow = MeshP.DOF;
+
+end
