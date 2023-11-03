ccf716d040b85c5176d51b1c9f70e9cf00c8c54e
Bruna <bccampos@uwaterloo.ca>
2023-11-03 19:44:45 -0400

    Add functions for computing plot contours

---
+++ Functions/Pre Processing/PlotVelAtt_Contour.m
@@ -0,0 +1,100 @@
+function PlotVelAtt_Contour(w, mat, vp1_dCS, vp2_dCS, vs1_dCS, vs2_dCS, attp1_dCS, attp2_dCS, atts1_dCS, atts2_dCS)
+% Contour plots for frequency vs velocity/attenuation vs material parameter
+% for dCS theory
+% ------------------------------------------------------------------------
+
+% initialize figure
+figure;
+tiledlayout(2,4);
+
+%% Plot velocity
+% fast P wave
+nexttile;
+contourf(mat, w, vp1_dCS, 20);
+hold on
+grid on
+title('Fast P wave');
+xlabel('Material parameter');
+ylabel('Frequency [Hz]');
+legend('Velocity [m/s]');
+hold off
+
+% slow P wave
+nexttile;
+contourf(mat, w, vp2_dCS, 20);
+hold on
+grid on
+title('Slow P wave');
+xlabel('Material parameter');
+ylabel('Frequency [Hz]');
+legend('Velocity [m/s]');
+hold off
+
+% fast S wave
+nexttile;
+contourf(mat, w, vs1_dCS, 20);
+hold on
+grid on
+title('Fast S wave');
+xlabel('Material parameter');
+ylabel('Frequency [Hz]');
+legend('Velocity [m/s]');
+hold off
+
+% slow S wave
+nexttile;
+contourf(mat, w, vs2_dCS, 20);
+hold on
+grid on
+title('Slow S wave');
+xlabel('Material parameter');
+ylabel('Frequency [Hz]');
+legend('Velocity [m/s]');
+hold off
+
+%% Plot attenuation
+% fast P wave
+nexttile;
+contourf(mat, w, attp1_dCS, 20);
+hold on
+grid on
+title('Fast P wave');
+xlabel('Material parameter');
+ylabel('Frequency [Hz]');
+legend('Attenuation [1/m]');
+hold off
+
+% slow P wave
+nexttile;
+contourf(mat, w, attp2_dCS, 20);
+hold on
+grid on
+title('Slow P wave');
+xlabel('Material parameter');
+ylabel('Frequency [Hz]');
+legend('Attenuation [1/m]');
+hold off
+
+% fast S wave
+nexttile;
+contourf(mat, w, atts1_dCS, 20);
+hold on
+grid on
+title('Fast S wave');
+xlabel('Material parameter');
+ylabel('Frequency [Hz]');
+legend('Attenuation [1/m]');
+hold off
+
+% slow S wave
+nexttile;
+contourf(mat, w, atts2_dCS, 20);
+hold on
+grid on
+title('Slow S wave');
+xlabel('Material parameter');
+ylabel('Frequency [Hz]');
+legend('Attenuation [1/m]');
+hold off
+
+end
