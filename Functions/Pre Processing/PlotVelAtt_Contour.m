80353381732fbe6e360b82a899a80054334013a2
Bruna <bccampos@uwaterloo.ca>
2023-11-07 09:31:01 -0500

    Add surf plot option and set x log scale in vel/att plots

--- Functions/Pre Processing/PlotVelAtt_Contour.m
+++ Functions/Pre Processing/PlotVelAtt_Contour.m
@@ -11,90 +11,106 @@ tiledlayout(2,4);
 % fast P wave
 nexttile;
 contourf(mat, w, vp1_dCS, 20);
+% surf(mat, w, vp1_dCS);
 hold on
 grid on
 title('Fast P wave');
 xlabel('Material parameter');
 ylabel('Frequency [Hz]');
 legend('Velocity [m/s]');
+set(gca, 'XScale', 'log');
 hold off
 
 % slow P wave
 nexttile;
 contourf(mat, w, vp2_dCS, 20);
+% surf(mat, w, vp2_dCS);
 hold on
 grid on
 title('Slow P wave');
 xlabel('Material parameter');
 ylabel('Frequency [Hz]');
 legend('Velocity [m/s]');
+set(gca, 'XScale', 'log');
 hold off
 
 % fast S wave
 nexttile;
 contourf(mat, w, vs1_dCS, 20);
+% surf(mat, w, vs1_dCS);
 hold on
 grid on
 title('Fast S wave');
 xlabel('Material parameter');
 ylabel('Frequency [Hz]');
 legend('Velocity [m/s]');
+set(gca, 'XScale', 'log');
 hold off
 
 % slow S wave
 nexttile;
 contourf(mat, w, vs2_dCS, 20);
+% surf(mat, w, vs2_dCS);
 hold on
 grid on
 title('Slow S wave');
 xlabel('Material parameter');
 ylabel('Frequency [Hz]');
 legend('Velocity [m/s]');
+set(gca, 'XScale', 'log');
 hold off
 
 %% Plot attenuation
 % fast P wave
 nexttile;
 contourf(mat, w, attp1_dCS, 20);
+% surf(mat, w, attp1_dCS);
 hold on
 grid on
 title('Fast P wave');
 xlabel('Material parameter');
 ylabel('Frequency [Hz]');
 legend('Attenuation [1/m]');
+set(gca, 'XScale', 'log');
 hold off
 
 % slow P wave
 nexttile;
 contourf(mat, w, attp2_dCS, 20);
+% surf(mat, w, attp2_dCS);
 hold on
 grid on
 title('Slow P wave');
 xlabel('Material parameter');
 ylabel('Frequency [Hz]');
 legend('Attenuation [1/m]');
+set(gca, 'XScale', 'log');
 hold off
 
 % fast S wave
 nexttile;
 contourf(mat, w, atts1_dCS, 20);
+% surf(mat, w, atts1_dCS);
 hold on
 grid on
 title('Fast S wave');
 xlabel('Material parameter');
 ylabel('Frequency [Hz]');
 legend('Attenuation [1/m]');
+set(gca, 'XScale', 'log');
 hold off
 
 % slow S wave
 nexttile;
 contourf(mat, w, atts2_dCS, 20);
+% surf(mat, w, atts2_dCS);
 hold on
 grid on
 title('Slow S wave');
 xlabel('Material parameter');
 ylabel('Frequency [Hz]');
 legend('Attenuation [1/m]');
+set(gca, 'XScale', 'log');
 hold off
 
 end
