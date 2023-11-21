947fe5875fd00769cd3a29dd28403bb5c4a3890a
Bruna <bccampos@uwaterloo.ca>
2023-11-21 17:30:45 -0500

    Update initVariables to account for all initial conditions

--- Functions/Pre Processing/initVariables.m
+++ Functions/Pre Processing/initVariables.m
@@ -5,19 +5,33 @@ function [Iteration, Plot] = initVariables(phi_u, phi_p, phi_n, MeshU, MeshP, Me
 % ------------------------------------------------------------------------
 
 %% Iteration data - time domain
-Iteration.u_old = zeros(MeshU.nDOF, 1); % solid displacement 
-Iteration.udot_old = zeros(MeshU.nDOF, 1); % solid velocity
-Iteration.p_old = zeros(MeshP.nDOF, 1); % pressure
+% initial condition
+Iteration.u_old = BC.initU; % solid displacement 
+Iteration.p_old = BC.initP; % pressure
+Iteration.udot_old = BC.initUdot; % solid velocity
+
+% initialize time derivatives
 Iteration.u2dot_old = zeros(MeshU.nDOF, 1); % solid acceleration
 Iteration.pdot_old = zeros(MeshP.nDOF, 1); % pressure gradient
+
+% initialize load vectors
 Iteration.fu_old = zeros(MeshU.nDOF, 1); % load vector
 Iteration.fp_old = zeros(MeshP.nDOF, 1); % flux vector
+
+% initialize variables for UPN model
 if contains(Control.PMmodel, 'UPN')
+    % initial porosity condition
+    Iteration.n_old = zeros(MeshN.nDOF, 1);
+    Iteration.n_old(:) = Material.n;
+    Iteration.ndot_old = zeros(MeshN.nDOF, 1);
+    % load vector
     Iteration.fn_old = zeros(MeshN.nDOF, 1); % flux vector
 end
+
+% initialize variables for UPU model
 if contains(Control.PMmodel, 'UPU')
-    Iteration.uf_old = zeros(MeshU.nDOF, 1); % fluid displacement
-    Iteration.ufdot_old = zeros(MeshU.nDOF, 1); % fluid velocity
+    Iteration.uf_old = BC.initUf; % fluid displacement
+    Iteration.ufdot_old = BC.initUfdot; % fluid velocity
     Iteration.uf2dot_old = zeros(MeshU.nDOF, 1); % fluid acceleration
     Iteration.ff_old = zeros(MeshU.nDOF, 1); % load vector
 end
@@ -37,12 +51,28 @@ Plot.uan_time = zeros(length(Plot.time),1); % analytic solid displacement
 Plot.u_time = zeros(length(Plot.time), 1); % solid displacement
 Plot.udot_time = zeros(length(Plot.time), 1); % solid velocity
 Plot.u2dot_time = zeros(length(Plot.time), 1); % solid acceleration
+
+% store initial conditions
+Plot.u_time(1,1) = BC.initU(Control.plotu,1);
+Plot.p_time(1,1) = BC.initP(Control.plotp,1);
+Plot.udot_time(1,1) = BC.initUdot(Control.plotu,1);
+
 if contains(Control.PMmodel, 'UPU')
     Plot.uf_time = zeros(length(Plot.time), 1); % fluid displacement
     Plot.ufdot_time = zeros(length(Plot.time), 1); % fluid velocity
     Plot.uf2dot_time = zeros(length(Plot.time), 1); % fluid acceleration
     Plot.ufan_time = zeros(length(Plot.time),1); % analytic fluid displacement
+    
+    % store initial conditions
+    Plot.uf_time(1,1) = BC.initUf(Control.plotu,1);
+    Plot.ufdot_time(1,1) = BC.initUfdot(Control.plotu,1);
 end
+
+% porosity
+if contains(Control.PMmodel, 'UPN')
+    Plot.n_time = zeros(length(Plot.time), 1);
+    Plot.n_time(1,1) = Material.n;
+end  
 
 %% Plot arrays - synthetic plots
 if Control.fixedDepthPlotON
@@ -65,6 +95,7 @@ Plot.udot_space = zeros(length(Control.plotu),1);
 Plot.u2dot_space = zeros(length(Control.plotu),1);
 Plot.pan_space = zeros(length(Control.plotp),1);
 Plot.uan_space = zeros(length(Control.plotu),1);
+
 if contains(Control.PMmodel, 'UPU')
     Plot.uf_space = zeros(length(Control.plotu),1);
     Plot.ufdot_space = zeros(length(Control.plotu),1);
@@ -102,57 +133,14 @@ end
 %% Plot arrays - frequency domain
 Plot.pF = zeros(length(Plot.time), 1); % fluid pressure
 Plot.uF = zeros(length(Plot.time), 1); % solid displacement
-Plot.uFdot = zeros(length(Plot.time), 1); % solid velocity
 
-%% Initial conditions
-% solid displacement
-if ~isempty(BC.initU)
-    Iteration.u_old = BC.initU;
-    Plot.u_time(1,1) = BC.initU(Control.plotu,1);
-    Plot.uF(1,1) = BC.initU(Control.plotu,1);
-end
+% store initial conditions
+Plot.uF(1,1) = BC.initU(Control.plotu,1);
+Plot.pF(1,1) = BC.initP(Control.plotp,1);
 
-% pressure
-if ~isempty(BC.initP)
-    Iteration.p_old = BC.initP;
-    Plot.p_time(1,1) = BC.initP(Control.plotp,1);
-    Plot.pF(1,1) = BC.initP(Control.plotp,1);
-end
-
-% fluid displacement
 if contains(Control.PMmodel, 'UPU')
-    if ~isempty(BC.initUf)
-        Iteration.uf_old = BC.initUf;
-        Plot.uf_time(1,1) = BC.initUf(Control.plotu,1);
-        Plot.ufF(1,1) = BC.initUf(Control.plotu,1);
-    end
-end
-
-% dynamic problems
-if contains(Control.PMmodel, 'Dyn')
-    % solid velocity
-    if ~isempty(BC.initUdot)
-        Iteration.udot_old = BC.initUdot;
-        Plot.udot_time(1,1) = BC.initUdot(Control.plotu,1);
-    end
-    % fluid velocity
-    if contains(Control.PMmodel, 'UPU')
-        if ~isempty(BC.initUfdot)
-            Iteration.ufdot_old = BC.initUfdot;
-            Plot.ufdot_time(1,1) = BC.initUfdot(Control.plotu,1);
-        end
-    end
-end
-
-% porosity
-if contains(Control.PMmodel, 'UPN')
-    % initial porosity condition
-    Iteration.n_old = zeros(MeshN.nDOF, 1);
-    Iteration.n_old(:) = Material.n;
-    Iteration.ndot_old = zeros(MeshN.nDOF, 1);
-    % plot porosity over time
-    Plot.n_time = zeros(length(Plot.time), 1);
-    Plot.n_time(1,1) = Material.n;
+    Plot.uFdot = zeros(length(Plot.time), 1); % solid velocity
+    Plot.ufF(1,1) = BC.initUf(Control.plotu,1);
 end
 
 end
