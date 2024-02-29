5795e32828144000d75f0b43d0428b00637a06b4
Bruna <bccampos@uwaterloo.ca>
2024-02-29 16:44:25 -0300

    Delete mode superposition variables from initVariables

--- Functions/Pre Processing/initVariables.m
+++ Functions/Pre Processing/initVariables.m
@@ -1,4 +1,4 @@
-function [Iteration, Plot] = initVariables(phi_u, phi_p, phi_n, MeshU, MeshP, MeshN, Material, Control, BC, M)
+function [Iteration, Plot] = initVariables(MeshU, MeshP, MeshN, Material, Control, BC, M)
 % ------------------------------------------------------------------------
 % Initialize variables for iteration and plot stages; store initial
 % conditions
@@ -109,47 +109,6 @@ if contains(Control.PMmodel, 'UPU')
     Plot.uf_space = zeros(length(Control.plotu),1);
     Plot.ufdot_space = zeros(length(Control.plotu),1);
     Plot.uf2dot_space = zeros(length(Control.plotu),1);
-end
-
-%% Iteration data - frequency domain
-Iteration.uF_old = zeros(MeshU.nDOF, 1); % displacement variable storage
-Iteration.uFdot_old = zeros(MeshU.nDOF, 1); % solid velocity
-Iteration.pF_old = zeros(MeshP.nDOF, 1); % pressure variable storage
-Iteration.uF2dot_old = zeros(MeshU.nDOF, 1); % solid acceleration
-Iteration.pFdot_old = zeros(MeshP.nDOF, 1); % pressure gradient
-
-% initial conditions for generalized displacements
-if Control.freqDomain
-    Iteration.xuF_old = (phi_u) \ Iteration.uF_old(BC.free_u);
-    Iteration.xuFdot_old = (phi_u) \ Iteration.uFdot_old(BC.free_u);
-    Iteration.xuF2dot_old = zeros(length(BC.free_u),1);
-    Iteration.xpF_old = (phi_p) \ Iteration.pF_old(BC.free_p);
-    Iteration.xpFdot_old = (phi_p) \ Iteration.pFdot_old(BC.free_p);
-    if contains(Control.PMmodel, 'UPN')
-        Iteration.nF_old = zeros(MeshN.nDOF, 1); % porosity variable storage
-        Iteration.nF_old(:) = Material.n;
-        Iteration.nFdot_old = zeros(MeshN.nDOF, 1); % porosity gradient
-        Iteration.xnF_old = (phi_n) \ Iteration.nF_old(BC.free_n);
-        Iteration.xnFdot_old = (phi_n) \ Iteration.nFdot_old(BC.free_n);
-    end
-    
-    if nargin > 9
-        Iteration.xuF_old = (phi_u.') * M(BC.free_u, BC.free_u) * Iteration.uF_old(BC.free_u);
-        Iteration.xuFdot_old = (phi_u.') * M(BC.free_u, BC.free_u) * Iteration.uFdot_old(BC.free_u);
-    end
-end
-
-%% Plot arrays - frequency domain
-Plot.pF = zeros(length(Plot.time), 1); % fluid pressure
-Plot.uF = zeros(length(Plot.time), 1); % solid displacement
-
-% store initial conditions
-Plot.uF(1,1) = BC.initU(Control.plotu,1);
-Plot.pF(1,1) = BC.initP(Control.plotp,1);
-
-if contains(Control.PMmodel, 'UPU')
-    Plot.uFdot = zeros(length(Plot.time), 1); % solid velocity
-    Plot.ufF(1,1) = BC.initUf(Control.plotu,1);
 end
 
 end
