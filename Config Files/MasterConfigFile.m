55d90da60f40065a00c6fafb4a9cbb83407fc660
Bruna <bccampos@uwaterloo.ca>
2024-02-29 18:36:23 -0300

    Add input ~ for meshfilename in master config file

--- Config Files/MasterConfigFile.m
+++ Config Files/MasterConfigFile.m
@@ -1,4 +1,4 @@
-function [Material, MeshU, MeshP, MeshN, BC, Control] = MasterConfigFile(config_dir, progress_on)
+function [Material, MeshU, MeshP, MeshN, BC, Control] = MasterConfigFile(config_dir, progress_on,~)
 % [PROBLEM TYPE, e.g., column consolidation]
 % Configuration File
 
