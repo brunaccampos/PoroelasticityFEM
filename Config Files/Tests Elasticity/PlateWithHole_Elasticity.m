5adc3df14cf4cb2e4638fe4f50ccee30dd898b42
Bruna <bccampos@uwaterloo.ca>
2024-02-29 18:32:33 -0300

    Add input ~ for meshfilename in elasticity examples

--- Config Files/Tests Elasticity/PlateWithHole_Elasticity.m
+++ Config Files/Tests Elasticity/PlateWithHole_Elasticity.m
@@ -1,4 +1,4 @@
-function [Material, MeshU, MeshP, MeshN, BC, Control] = PlateWithHole_Elasticity(config_dir, progress_on)
+function [Material, MeshU, MeshP, MeshN, BC, Control] = PlateWithHole_Elasticity(config_dir, progress_on,~)
 % 2D elasticity problem - steady case
 % Configuration file
 % ------------------------------------------------------------------------
