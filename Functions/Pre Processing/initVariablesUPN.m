9e513c122655591c5f20aa54f454061e6c064ebe
Bruna <bccampos@uwaterloo.ca>
2024-04-15 17:06:05 -0400

    Add functions for init vars separately for each model

---
+++ Functions/Pre Processing/initVariablesUPN.m
@@ -0,0 +1,73 @@
+function [Iteration, Plot] = initVariablesUPN(MeshU, MeshP, MeshN, Material, Control, BC)
+% ------------------------------------------------------------------------
+% Initialize variables for Iteration and Plot structures
+% Model u-p-n (solid displacement, fluid pressure, porosity)
+% ------------------------------------------------------------------------
+
+%% Iteration data - time domain
+% initial condition
+Iteration.u_old = BC.initU; % solid displacement 
+Iteration.p_old = BC.initP; % pressure
+Iteration.udot_old = BC.initUdot; % solid velocity
+Iteration.n_old = zeros(MeshN.nDOF, 1); % porosity
+Iteration.n_old(:) = Material.n;
+
+% time derivatives
+Iteration.u2dot_old = zeros(MeshU.nDOF, 1); % solid acceleration
+Iteration.pdot_old = zeros(MeshP.nDOF, 1); % pressure gradient
+Iteration.ndot_old = zeros(MeshN.nDOF, 1); % porosity variation
+
+% load vectors
+Iteration.fu_old = zeros(MeshU.nDOF, 1); % load vector
+Iteration.fp_old = zeros(MeshP.nDOF, 1); % flux vector
+Iteration.fn_old = zeros(MeshN.nDOF, 1); % flux vector
+
+%% Plot arrays - time domain
+if isfield(Control, 'dtmin') 
+    time1 = (0:Control.dtmin:Control.tlim);
+    time2 = (Control.tlim + Control.dt:Control.dt:Control.tend);
+    Plot.time = [time1, time2];
+else
+    Plot.time = (0:Control.dt:Control.tend);
+end
+
+% FE solution
+Plot.p_time = zeros(length(Plot.time), 1); % fluid pressure
+Plot.u_time = zeros(length(Plot.time), 1); % solid displacement
+Plot.udot_time = zeros(length(Plot.time), 1); % solid velocity
+Plot.u2dot_time = zeros(length(Plot.time), 1); % solid acceleration
+
+% analytical solution
+Plot.pan_time = zeros(length(Plot.time), 1); % analytic fluid pressure
+Plot.uan_time = zeros(length(Plot.time),1); % analytic solid displacement
+
+% initial conditions for FE solution
+Plot.u_time(1,1) = BC.initU(Control.plotu,1);
+Plot.p_time(1,1) = BC.initP(Control.plotp,1);
+Plot.udot_time(1,1) = BC.initUdot(Control.plotu,1);
+Plot.n_time = zeros(length(Plot.time), 1);
+Plot.n_time(1,1) = Material.n;
+    
+% initial conditions for analytical solution
+if Control.plotansol
+    aux1 = Control.p_an(0);
+    aux2 = Control.u_an(0);
+    Plot.pan_time(1,1) = aux1(Control.plotp);
+    Plot.uan_time(1,1) = aux2(Control.plotu);
+end
+
+%% Plot arrays - synthetics
+Plot.u_synthetic = zeros(length(Plot.time), length(Control.ploturow)); % solid displacement
+Plot.udot_synthetic = zeros(length(Plot.time), length(Control.ploturow)); % solid velocity
+Plot.p_synthetic = zeros(length(Plot.time), length(Control.plotprow)); % fluid pressure
+Plot.n_synthetic = zeros(length(Plot.time), length(Control.plotprow)); % porosity
+
+%% Plot arrays - space
+Plot.p_space = zeros(length(Control.plotp),1);
+Plot.u_space = zeros(length(Control.plotu),1);
+Plot.udot_space = zeros(length(Control.plotu),1);
+Plot.u2dot_space = zeros(length(Control.plotu),1);
+Plot.pan_space = zeros(length(Control.plotp),1);
+Plot.uan_space = zeros(length(Control.plotu),1);
+
+end
---
+++ Functions/Pre Processing/initVariablesUPU.m
@@ -0,0 +1,83 @@
+function [Iteration, Plot] = initVariablesUPU(MeshU, MeshP, ~, ~, Control, BC)
+% ------------------------------------------------------------------------
+% Initialize variables for Iteration and Plot structures
+% Model u-p-U (solid displacement, fluid pressure, fluid displacement)
+% ------------------------------------------------------------------------
+
+%% Iteration data - time domain
+% initial condition
+Iteration.u_old = BC.initU; % solid displacement 
+Iteration.p_old = BC.initP; % pressure
+Iteration.udot_old = BC.initUdot; % solid velocity
+Iteration.uf_old = BC.initUf; % fluid displacement
+Iteration.ufdot_old = BC.initUfdot; % fluid velocity
+
+% time derivatives
+Iteration.u2dot_old = zeros(MeshU.nDOF, 1); % solid acceleration
+Iteration.pdot_old = zeros(MeshP.nDOF, 1); % pressure gradient
+Iteration.uf2dot_old = zeros(MeshU.nDOF, 1); % fluid acceleration
+
+% load vectors
+Iteration.fu_old = zeros(MeshU.nDOF, 1); % load vector
+Iteration.fp_old = zeros(MeshP.nDOF, 1); % flux vector
+Iteration.ff_old = zeros(MeshU.nDOF, 1); % load vector
+
+%% Plot arrays - time domain
+if isfield(Control, 'dtmin') 
+    time1 = (0:Control.dtmin:Control.tlim);
+    time2 = (Control.tlim + Control.dt:Control.dt:Control.tend);
+    Plot.time = [time1, time2];
+else
+    Plot.time = (0:Control.dt:Control.tend);
+end
+
+% FE solution
+Plot.p_time = zeros(length(Plot.time), 1); % fluid pressure
+Plot.u_time = zeros(length(Plot.time), 1); % solid displacement
+Plot.udot_time = zeros(length(Plot.time), 1); % solid velocity
+Plot.u2dot_time = zeros(length(Plot.time), 1); % solid acceleration
+Plot.uf_time = zeros(length(Plot.time), 1); % fluid displacement
+Plot.ufdot_time = zeros(length(Plot.time), 1); % fluid velocity
+Plot.uf2dot_time = zeros(length(Plot.time), 1); % fluid acceleration
+
+% analytical solution
+Plot.pan_time = zeros(length(Plot.time), 1); % analytic fluid pressure (quasi-steady case)
+Plot.uan_time = zeros(length(Plot.time),1); % analytic solid displacement
+Plot.ufan_time = zeros(length(Plot.time),1); % analytic fluid displacement
+
+if Control.plotansol
+    aux1 = Control.p_an(0);
+    aux2 = Control.u_an(0);
+    aux3 = Control.uf_an(0);
+    Plot.pan_time(1,1) = aux1(Control.plotp);
+    Plot.uan_time(1,1) = aux2(Control.plotu);
+    Plot.ufan_time(1,1) = aux3(Control.plotu);
+end
+
+% initial conditions for FE solution
+Plot.u_time(1,1) = BC.initU(Control.plotu,1);
+Plot.p_time(1,1) = BC.initP(Control.plotp,1);
+Plot.udot_time(1,1) = BC.initUdot(Control.plotu,1);
+Plot.uf_time(1,1) = BC.initUf(Control.plotu,1);
+Plot.ufdot_time(1,1) = BC.initUfdot(Control.plotu,1);
+    
+%% Plot arrays - synthetics
+Plot.u_synthetic = zeros(length(Plot.time), length(Control.ploturow)); % solid displacement
+Plot.udot_synthetic = zeros(length(Plot.time), length(Control.ploturow)); % solid velocity
+Plot.p_synthetic = zeros(length(Plot.time), length(Control.plotprow)); % fluid pressure
+Plot.uf_synthetic = zeros(length(Plot.time), length(Control.ploturow)); % fluid displacement
+Plot.ufdot_synthetic = zeros(length(Plot.time), length(Control.ploturow)); % fluid pressure
+
+%% Plot arrays - space
+Plot.p_space = zeros(length(Control.plotp),1);
+Plot.u_space = zeros(length(Control.plotu),1);
+Plot.udot_space = zeros(length(Control.plotu),1);
+Plot.u2dot_space = zeros(length(Control.plotu),1);
+Plot.pan_space = zeros(length(Control.plotp),1);
+Plot.uan_space = zeros(length(Control.plotu),1);
+Plot.uf_space = zeros(length(Control.plotu),1);
+Plot.ufdot_space = zeros(length(Control.plotu),1);
+Plot.uf2dot_space = zeros(length(Control.plotu),1);
+Plot.ufan_space = zeros(length(Control.plotu),1);
+
+end
