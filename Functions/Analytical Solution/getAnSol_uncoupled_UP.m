c3d3e1eeafffa572b366c128019dd34bc4e448e2
Bruna <bccampos@uwaterloo.ca>
2024-05-15 14:31:49 -0400

    Rename analytical solution functions

--- Functions/Analytical Solution/getAnSol_uncoupled_UP.m
+++ Functions/Analytical Solution/getAnSol_uncoupled_UP.m
@@ -1,4 +1,4 @@
-function [p_an, u_an, uf_an] = getAnSol_uncoupled(Control, ~, ~, ~, ~)
+function [p_an, u_an] = getAnSol_uncoupled_UP(Control, ~, ~, ~, ~)
 % ------------------------------------------------------------------------
 % Return analytical solution for uncoupled problem (e.g. elasticity, heat
 % transfer
@@ -6,6 +6,5 @@ function [p_an, u_an, uf_an] = getAnSol_uncoupled(Control, ~, ~, ~, ~)
 
 p_an = Control.p_an(Control.t);
 u_an = Control.u_an(Control.t);
-uf_an = Control.uf_an(Control.t);
 
 end
