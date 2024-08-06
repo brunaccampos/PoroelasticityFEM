5d7230282151f4ce0be3fc18cb4a5db6d1a21284
Bruna <bccampos@uwaterloo.ca>
2024-08-06 11:52:24 -0400

    Add viscosity correction factor to BT wave equations

--- Functions/Wave Propagation/getWaveCoeffs_BT.m
+++ Functions/Wave Propagation/getWaveCoeffs_BT.m
@@ -20,32 +20,36 @@ deltaf = Material.deltaf; % compliance coefficient fluid
 eta0 = Material.eta0; % porosity
 k = Material.k; % intrinsic permeability
 
+wc = muf*eta0/k/rhof;
+F = sqrt(1+0.5*1i*w./wc); % viscosity correction factor
+% F = ones(length(w));
+
 %% Auxiliar constants
 vps2 = (Ks+4*mus/3)/rhos;
-aps = muf*eta0^2/(k*rhos*(1-eta0));
+aps = muf*eta0^2/(k*rhos*(1-eta0)).*F;
 cps = rho12/(1-eta0)/rhos;
 dps = Ks*deltas/rhos/(1-eta0);
 hps = Ks*deltaf/rhos/(1-eta0);
 
 c2 = Kf/rhof;
-bpf = muf*eta0/k/rhof;
+bpf = muf*eta0/k/rhof.*F;
 cpf = rho12/eta0/rhof;
 dpf = Kf*deltas/rhof/eta0;
 hpf = Kf*deltaf/rhof/eta0;
 
 css = mus/rhos;
-dss = muf*eta0^2/(k*rhos*(1-eta0));
-dsf = muf*eta0/k/rhof;
+dss = muf*eta0^2/(k*rhos*(1-eta0)).*F;
+dsf = muf*eta0/k/rhof.*F;
 
 %% Compressional P wave
-Ap_BT = (c2*vps2-vps2*hpf-dps*c2+dps*hpf-hps*dpf)*ones(1,length(w));
-Bp_BT = (-c2+hpf-vps2+vps2*cpf+dps-dps*cpf+cps*c2-cps*hpf+hps*cpf+cps*dpf)*w.^2 + ...
-    (-vps2*bpf+dps*bpf-aps*c2+aps*hpf-hps*bpf-aps*dpf)*1i*w;
-Cp_BT = (1-cps-cpf)*w.^4+(aps+bpf)*1i*w.^3;
+Ap_BT = (c2*vps2-vps2*hpf-dps*c2+dps*hpf-hps*dpf).*ones(1,length(w));
+Bp_BT = (-c2+hpf-vps2+vps2*cpf+dps-dps*cpf+cps*c2-cps*hpf+hps*cpf+cps*dpf).*w.^2 + ...
+    (-vps2.*bpf+dps.*bpf-aps.*c2+aps.*hpf-hps.*bpf-aps.*dpf).*1i.*w;
+Cp_BT = (1-cps-cpf)*w.^4+(aps+bpf)*1i.*w.^3;
 
 %% Shear S wave
 As_BT = zeros(1,length(w));
-Bs_BT = (-css+css*cpf)*w.^2-dsf*css*1i*w;
-Cs_BT = (1-cps-cpf)*w.^4+(dss+dsf)*1i*w.^3;
+Bs_BT = (-css+css*cpf).*w.^2-dsf.*css*1i.*w;
+Cs_BT = (1-cps-cpf).*w.^4+(dss+dsf)*1i.*w.^3;
 
 end
