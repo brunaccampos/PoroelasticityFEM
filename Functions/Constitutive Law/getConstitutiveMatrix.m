71ed1a67749a497fc769dad4a5cbc9d0b401d6e2
bccampos@uwaterloo.ca <bcarol!W22>
2024-08-29 18:38:54 -0400

    Fix constitutive matrix for material at element level

--- Functions/Constitutive Law/getConstitutiveMatrix.m
+++ Functions/Constitutive Law/getConstitutiveMatrix.m
@@ -1,4 +1,12 @@
-function [C] = getConstitutiveMatrix(Material, Mesh)
+function [C] = getConstitutiveMatrix(nMat, Material, Mesh)
+% ------------------------------------------------------------------------
+% Return linear elastic constitutive matrix for solid constituent
+% ------------------------------------------------------------------------
+
+% Material properties
+E = Material.M(nMat).E;
+nu = Material.M(nMat).nu;
+
 % Compute constitutive matrix
 nsd = Mesh.nsd;
 
@@ -6,29 +14,29 @@ switch nsd
     case 1
     switch Material.constLaw
         case 'PlaneStress'
-            C = Material.E;
+            C = E;
         case 'PlaneStrain'
-            C = Material.E*(1-Material.nu)/((1+Material.nu)*(1-2*Material.nu));
+            C = E*(1-nu)/((1+nu)*(1-2*nu));
     end
     case 2
         switch Material.constLaw
             case 'PlaneStress'
                 C = zeros(3,3);
-                aux = Material.E/(1-Material.nu^2);
+                aux = E/(1-nu^2);
                 C(1,1) = 1;
                 C(2,2) = 1;
-                C(3,3) = (1-Material.nu)/2;
-                C(1,2) = Material.nu;
-                C(2,1) = Material.nu;
+                C(3,3) = (1-nu)/2;
+                C(1,2) = nu;
+                C(2,1) = nu;
                 C = C*aux;
             case 'PlaneStrain'
                 C = zeros(3,3);
-                aux = Material.E/((1+Material.nu) * (1-2*Material.nu));
-                C(1,1) = 1 - Material.nu;
-                C(2,2) = 1 - Material.nu;
-                C(3,3) = (1-2*Material.nu)/2;
-                C(1,2) = Material.nu;
-                C(2,1) = Material.nu;
+                aux = E/((1+nu) * (1-2*nu));
+                C(1,1) = 1 - nu;
+                C(2,2) = 1 - nu;
+                C(3,3) = (1-2*nu)/2;
+                C(1,2) = nu;
+                C(2,1) = nu;
                 C = C*aux;
         end
 end
