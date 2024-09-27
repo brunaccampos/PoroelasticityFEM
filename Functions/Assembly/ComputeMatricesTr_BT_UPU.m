461cbdb689756ac13fe45f925f67bbbfc975d9bf
Bruna <bccampos@uwaterloo.ca>
2024-09-27 10:16:42 -0400

    Rename assembly functions

---
+++ Functions/Assembly/ComputeMatricesTr_BT_UPU.m
@@ -0,0 +1,206 @@
+function [Kss, Ksp, Csf, Css, Kpf, Kps, Kpp, Kfp, Cff, Cfs] = ComputeMatricesTr4_Biot_UPU(Material, MeshU, MeshP, QuadU, QuadP)
+% ------------------------------------------------------------------------
+% Compute System Matrices for transient simulation
+% ------------------------------------------------------------------------
+% Input parameters: Material, Mesh, Control, Quad
+% Output matrices: Kss, Ksp, Csf, Css, Kpf, Kps, Kpp, Kfp, Cff, Cfs
+% ------------------------------------------------------------------------
+
+ne = MeshU.ne; % number of elements
+nqU = QuadU.nq; % total number of integration points
+nqP = QuadP.nq;
+
+%% Initialize global matrices
+% initialize vector sizes
+% u-u
+rowu = zeros(ne*MeshU.nDOFe^2,1);
+colu = zeros(ne*MeshU.nDOFe^2,1);
+% p-p
+rowp = zeros(ne*MeshP.nDOFe^2,1);
+colp = zeros(ne*MeshP.nDOFe^2,1);
+% u-p
+rowup = zeros(ne*MeshU.nDOFe*MeshP.nDOFe,1);
+colup = zeros(ne*MeshU.nDOFe*MeshP.nDOFe,1);
+
+Cssvec = zeros(ne*MeshU.nDOFe^2,1);
+Csfvec = zeros(ne*MeshU.nDOFe*MeshU.nDOFe,1);
+Cffvec = zeros(ne*MeshU.nDOFe^2,1);
+
+Kssvec = zeros(ne*MeshU.nDOFe^2,1);
+Kspvec = zeros(ne*MeshU.nDOFe*MeshP.nDOFe,1);
+Kppvec = zeros(ne*MeshP.nDOFe^2,1);
+Kfpvec = zeros(ne*MeshU.nDOFe*MeshP.nDOFe,1);
+
+% DOF counter
+count_u = 1;
+count_p = 1;
+count_up = 1;
+
+%% Coupled matrices
+for e = 1:ne
+    % element material type
+    nMat = MeshU.MatList(e); % element material type
+    % constitutive matrix
+    C = getConstitutiveMatrix(nMat, Material, MeshU);
+
+    % element connectivity
+    connu_e = MeshU.conn(e,:);
+    connu_e = reshape(connu_e',MeshU.nne,[]);
+    connp_e = MeshP.conn(e,:);
+    connp_e = reshape(connp_e',MeshP.nne,[]);
+
+    % element DOF numbers
+    dofu_e = MeshU.DOF(connu_e,:);
+    dofu_e = reshape(dofu_e',MeshU.nDOFe,[]);
+    dofp_e = MeshP.DOF(connp_e,:);
+    dofp_e = reshape(dofp_e',MeshP.nDOFe,[]);
+
+    % global coordinates
+    gcoordsU = MeshU.coords(connu_e,:);
+    gcoordsP = MeshP.coords(connp_e,:);
+
+    % initialize local matrices
+    Css_e = zeros(MeshU.nDOFe, MeshU.nDOFe);
+    Csf_e = zeros(MeshU.nDOFe, MeshU.nDOFe);
+    Cff_e = zeros(MeshU.nDOFe, MeshU.nDOFe);
+    
+    Kss_e = zeros(MeshU.nDOFe, MeshU.nDOFe);
+    Ksp_e = zeros(MeshU.nDOFe, MeshP.nDOFe);
+    Kfp_e = zeros(MeshU.nDOFe, MeshP.nDOFe);
+    Kpp_e = zeros(MeshP.nDOFe, MeshP.nDOFe);
+
+    % loop over integration points - Displacement
+    for ip = 1:nqU
+
+        % N matrices
+        Nu = getN(MeshU, QuadU, ip);
+
+        % N derivatives
+        dNu = getdN(MeshU, QuadU, ip);
+        dNp = getdN(MeshP, QuadU, ip);
+
+        % Jacobian matrix
+        Ju = dNu*gcoordsU;
+        Jp = dNp*gcoordsP;
+        % Jacobian determinant
+        Jdet = det(Jp);
+
+        % B matrices
+        Bu = Ju\dNu;
+
+        % changing matrices to Voigt form
+        NuVoigt = getNVoigt(MeshU, Nu);
+        BuVoigt = getBVoigt(MeshU, Bu);
+
+        % assemble local matrices
+        Kss_e = Kss_e + (BuVoigt.') * C * BuVoigt * Material.t * Jdet * QuadU.w(ip,1);
+        
+        Css_e = Css_e + Material.M(nMat).eta0^2/Material.M(nMat).kf * (NuVoigt.') * NuVoigt * Material.t * Jdet * QuadU.w(ip,1);
+        Csf_e = Csf_e + Material.M(nMat).eta0^2/Material.M(nMat).kf * (NuVoigt.') * NuVoigt * Material.t * Jdet * QuadU.w(ip,1);
+        Cff_e = Cff_e + Material.M(nMat).eta0^2/Material.M(nMat).kf * (NuVoigt.') * NuVoigt * Material.t * Jdet * QuadU.w(ip,1);
+    end
+ 
+    % loop over integration points - Displacement
+    for ip = 1:nqP
+
+        % N matrices
+        Np = getN(MeshP, QuadP, ip);
+
+        % N derivatives
+        dNu = getdN(MeshU, QuadP, ip);
+        dNp = getdN(MeshP, QuadP, ip);
+
+        % Jacobian matrix
+        Ju = dNu*gcoordsU;
+        Jp = dNp*gcoordsP;
+        % Jacobian determinant
+        Jdet = det(Jp);
+
+        % B matrices
+        Bu = Ju\dNu;
+
+        % changing matrices to Voigt form
+        NpVoigt = getNVoigt(MeshP, Np);
+        BuVoigt = getBVoigt(MeshU, Bu);
+
+        % assemble local matrices
+        Kpp_e = Kpp_e + Material.M(nMat).Minv * (NpVoigt.') * NpVoigt * Material.t * Jdet * QuadP.w(ip,1);
+        Ksp_e = Ksp_e + (Material.M(nMat).alpha - Material.M(nMat).eta0) * (BuVoigt.') * Material.m * NpVoigt * Material.t * Jdet * QuadP.w(ip,1);
+        Kfp_e = Kfp_e + Material.M(nMat).eta0 * (BuVoigt.') * Material.m * NpVoigt * Material.t * Jdet * QuadP.w(ip,1);
+    end
+
+    % lumped element damping matrix
+    if Material.lumpedDamping
+        Css_eDiag = zeros(MeshU.nDOFe, MeshU.nDOFe);
+        Cff_eDiag = zeros(MeshU.nDOFe, MeshU.nDOFe);
+        Csf_eDiag = zeros(MeshU.nDOFe, MeshU.nDOFe);
+        for k = 1:MeshU.nDOFe
+            Css_eDiag(k,k) = sum(Css_e(k,:));
+            Cff_eDiag(k,k) = sum(Cff_e(k,:));
+            Csf_eDiag(k,k) = sum(Csf_e(k,:));
+        end
+        Css_e = Css_eDiag;
+        Cff_e = Cff_eDiag;
+        Csf_e = Csf_eDiag;
+    end
+    
+    % vectorized matrices
+    count_u = count_u + MeshU.nDOFe^2;
+    count_p = count_p + MeshP.nDOFe^2;
+    count_up = count_up + MeshU.nDOFe*MeshP.nDOFe;
+    
+    Css_e = reshape(Css_e, [MeshU.nDOFe^2,1]);
+    Csf_e = reshape(Csf_e, [MeshU.nDOFe^2,1]);
+    Cff_e = reshape(Cff_e, [MeshU.nDOFe^2,1]);
+    
+    Kss_e = reshape(Kss_e, [MeshU.nDOFe^2,1]);
+    Ksp_e = reshape(Ksp_e, [MeshU.nDOFe*MeshP.nDOFe,1]);
+    Kfp_e = reshape(Kfp_e, [MeshU.nDOFe*MeshP.nDOFe,1]);
+    Kpp_e = reshape(Kpp_e, [MeshP.nDOFe^2,1]);
+    
+    % u-u
+    rowmatrix_u = dofu_e*ones(1,MeshU.nDOFe);
+    rowu_e = reshape(rowmatrix_u, [MeshU.nDOFe^2,1]);
+    colu_e = reshape(rowmatrix_u', [MeshU.nDOFe^2,1]);
+    % p-p
+    rowmatrix_p = dofp_e*ones(1,MeshP.nDOFe);
+    rowp_e = reshape(rowmatrix_p, [MeshP.nDOFe^2,1]);
+    colp_e = reshape(rowmatrix_p', [MeshP.nDOFe^2,1]);
+    % u-p
+    rowup_e = reshape(dofu_e*ones(1,MeshP.nDOFe),[MeshU.nDOFe*MeshP.nDOFe,1]);
+    colup_e = reshape(ones(MeshU.nDOFe,1)*dofp_e.',[MeshU.nDOFe*MeshP.nDOFe,1]);
+
+    Cssvec(count_u-MeshU.nDOFe^2:count_u-1) = Css_e;
+    Csfvec(count_u-MeshU.nDOFe^2:count_u-1) = Csf_e;
+    Cffvec(count_u-MeshU.nDOFe^2:count_u-1) = Cff_e;
+
+    Kssvec(count_u-MeshU.nDOFe^2:count_u-1) = Kss_e;
+    Kspvec(count_up-MeshU.nDOFe*MeshP.nDOFe:count_up-1) = Ksp_e;
+    Kfpvec(count_up-MeshU.nDOFe*MeshP.nDOFe:count_up-1) = Kfp_e;
+    Kppvec(count_p-MeshP.nDOFe^2:count_p-1) = Kpp_e;
+
+    % u-u
+    rowu(count_u-MeshU.nDOFe^2:count_u-1) = rowu_e;
+    colu(count_u-MeshU.nDOFe^2:count_u-1) = colu_e;
+    % p-p
+    rowp(count_p-MeshP.nDOFe^2:count_p-1) = rowp_e;
+    colp(count_p-MeshP.nDOFe^2:count_p-1) = colp_e;
+    % u-p
+    rowup(count_up-MeshU.nDOFe*MeshP.nDOFe:count_up-1) = rowup_e;
+    colup(count_up-MeshU.nDOFe*MeshP.nDOFe:count_up-1) = colup_e;
+end
+
+% sparse matrices
+Css = sparse(rowu, colu, Cssvec, MeshU.nDOF, MeshU.nDOF);
+Csf = sparse(rowu, colu, Csfvec, MeshU.nDOF, MeshU.nDOF);
+Cfs = Csf.';
+Cff = sparse(rowu, colu, Cffvec, MeshU.nDOF, MeshU.nDOF);
+
+Kss = sparse(rowu, colu, Kssvec, MeshU.nDOF, MeshU.nDOF);
+Ksp = sparse(rowup, colup, Kspvec, MeshU.nDOF, MeshP.nDOF);
+Kfp = sparse(rowup, colup, Kfpvec, MeshU.nDOF, MeshP.nDOF);
+Kpp = sparse(rowp, colp, Kppvec, MeshP.nDOF, MeshP.nDOF);
+Kps = Ksp.';
+Kpf = Kfp.';
+
+end
