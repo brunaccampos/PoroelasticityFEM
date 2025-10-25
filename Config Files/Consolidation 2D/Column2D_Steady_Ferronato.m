aad39dc0e941e4f471e79910443346ee97b44f35
bccampos@uwaterloo.ca <bcarol!W22>
2025-10-25 15:17:26 -0400

    Update headers config files column 2D

--- Config Files/Consolidation 2D/Column2D_Steady_Ferronato.m
+++ Config Files/Consolidation 2D/Column2D_Steady_Ferronato.m
@@ -1,3 +1,6 @@
+% SPDX-FileCopyrightText: Copyright (c) 2022-2024 Bruna Campos
+% SPDX-License-Identifier: GPL-3.0-or-later
+
 function [Material, MeshU, MeshP, MeshN, BC, Control] = Column2D_Steady_Ferronato(config_dir, progress_on,~,~)
 % Column Consolidation 1D simulation
 % Configuration File
