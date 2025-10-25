506c9c309820c8e08f8a8e4398aaae9e42a1550d
bccampos@uwaterloo.ca <bcarol!W22>
2025-10-25 15:29:29 -0400

    Update header master config file

--- Config Files/MasterConfigFile.m
+++ Config Files/MasterConfigFile.m
@@ -1,3 +1,6 @@
+% SPDX-FileCopyrightText: Copyright (c) 2022-2024 Bruna Campos
+% SPDX-License-Identifier: GPL-3.0-or-later
+
 function [Material, MeshU, MeshP, MeshN, BC, Control] = MasterConfigFile(config_dir, progress_on,~,~)
 % [PROBLEM TYPE, e.g., column consolidation]
 % Configuration File
