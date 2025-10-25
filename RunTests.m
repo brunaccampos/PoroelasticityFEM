bff66076a0e86c39a87b663ca9a2013d680a544a
bccampos@uwaterloo.ca <bcarol!W22>
2025-10-25 14:57:43 -0400

    Update header runsim and runtests

--- RunTests.m
+++ RunTests.m
@@ -13,7 +13,7 @@
 % You should have received a copy of the GNU General Public License
 % along with this program.  If not, see <https://www.gnu.org/licenses/>.
 %
-% Copyright (C) 2025 Bruna Campos
+% Copyright (C) 2022-2024 Bruna Campos
 
 % ------------------------------------------------------------------------
 % Porous Media Simulation - Tests
